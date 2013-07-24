fea0596692da365b70be4edf90d94ea2a23098a6
Konstantinos Paparas <kelsos86@gmail.com>
2013-07-24 09:19:56 +0300

    Fixes issue with an exception during command execution.

--- src/main/java/com/kelsos/mbrc/commands/model/UpdateVolume.java
+++ src/main/java/com/kelsos/mbrc/commands/model/UpdateVolume.java
@@ -4,12 +4,13 @@ import com.google.inject.Inject;
 import com.kelsos.mbrc.interfaces.ICommand;
 import com.kelsos.mbrc.interfaces.IEvent;
 import com.kelsos.mbrc.model.MainDataModel;
+import org.codehaus.jackson.node.IntNode;
 
 public class UpdateVolume implements ICommand {
     @Inject
     MainDataModel model;
     @Override
     public void execute(IEvent e) {
-        model.setVolume((Integer)e.getData());
+        model.setVolume(((IntNode)e.getData()).asInt());
     }
 }
