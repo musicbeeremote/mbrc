d171b27288e72c4c6963c3d94322f64a6042afdb
Konstantinos Paparas <kelsos86@gmail.com>
2013-07-25 00:15:02 +0300

    Reformating code of SocketService

--- src/main/java/com/kelsos/mbrc/services/SocketService.java
+++ src/main/java/com/kelsos/mbrc/services/SocketService.java
@@ -21,198 +21,162 @@ import java.net.SocketException;
 import java.net.SocketTimeoutException;
 
 @Singleton
-public class SocketService
-{
+public class SocketService {
 
-	private Bus bus;
-	private SettingsManager settingsManager;
+    public static final int MAX_RETRIES = 5;
+    private static int numOfRetries;
+    private Bus bus;
+    private SettingsManager settingsManager;
     private ObjectMapper mapper;
+    private boolean shouldStop;
+    private Socket clSocket;
+    private PrintWriter output;
+    private Thread cThread;
+    private DelayTimer cTimer;
+    private DelayTimer.TimerFinishEvent timerFinishEvent = new DelayTimer.TimerFinishEvent() {
+        public void onTimerFinish() {
+            cThread = new Thread(new socketConnection());
+            cThread.start();
+            numOfRetries++;
+        }
+    };
 
-	private static int numOfRetries;
-	public static final int MAX_RETRIES = 5;
-	private boolean shouldStop;
-
-	private Socket clSocket;
-
-	private PrintWriter output;
-
-	private Thread cThread;
-
-	private DelayTimer cTimer;
-
-	@Inject
-	public SocketService(SettingsManager settingsManager, Bus bus, ObjectMapper mapper)
-	{
-		this.bus = bus;
-		this.settingsManager = settingsManager;
+    @Inject
+    public SocketService(SettingsManager settingsManager, Bus bus, ObjectMapper mapper) {
+        this.bus = bus;
+        this.settingsManager = settingsManager;
         this.mapper = mapper;
 
-		cTimer = new DelayTimer(2500, timerFinishEvent);
-		numOfRetries = 0;
-		shouldStop = false;
-		SocketManager(SocketAction.START);
-	}
+        cTimer = new DelayTimer(2500, timerFinishEvent);
+        numOfRetries = 0;
+        shouldStop = false;
+        SocketManager(SocketAction.START);
+    }
 
-	private DelayTimer.TimerFinishEvent timerFinishEvent = new DelayTimer.TimerFinishEvent()
-	{
-		public void onTimerFinish()
-		{
-			cThread = new Thread(new socketConnection());
-			cThread.start();
-			numOfRetries++;
-		}
-	};
-
-	private boolean cThreadIsAlive()
-	{
-		return cThread != null && cThread.isAlive();
-	}
-
-	public void SocketManager(SocketAction action)
-	{
-		switch (action)
-		{
-			case RESET:
-				cleanupSocket();
-				if (cThread != null) cThread.interrupt();
-				cThread = null;
-				shouldStop = false;
-				numOfRetries = 0;
-				cTimer.start();
-				break;
-			case START:
-				if (sIsConnected() || cThreadIsAlive()) return;
-				else if (!sIsConnected() && cThreadIsAlive())
-				{
-					cThread.interrupt();
-					cThread = null;
-				}
-				cTimer.start();
-				break;
-			case RETRY:
-				cleanupSocket();
-				if(cThread != null) cThread.interrupt();
-				cThread=null;
-				if(shouldStop)
-				{
-					shouldStop = false;
-					numOfRetries = 0;
-					return;
-				}
-				cTimer.start();
-				break;
-			case STOP:
-				shouldStop = true;
-				break;
-		}
-	}
-
-	private void cleanupSocket()
-	{
-		if (!sIsConnected())
-			return;
-		try
-		{
-			if (output != null)
-			{
-				output.flush();
-				output.close();
-				output = null;
-			}
-			clSocket.close();
-			clSocket = null;
-		} catch (IOException ignore)
-		{
-
-		}
-	}
-
-	/**
-	 * Returns true if the socket is not null and it is connected, false in any other case.
-	 *
-	 * @return Boolean
-	 */
-	private boolean sIsConnected()
-	{
-		return clSocket != null && clSocket.isConnected();
-	}
-
-	public void sendData(SocketMessage message)
-	{
-		try
-		{
-			if (sIsConnected())
-				output.println(mapper.writeValueAsString(message) + Const.NEWLINE);
-		} catch (Exception ignored)
-		{
+    public void SocketManager(SocketAction action) {
+        switch (action) {
+            case RESET:
+                cleanupSocket();
+                if (cThread != null) cThread.interrupt();
+                cThread = null;
+                shouldStop = false;
+                numOfRetries = 0;
+                cTimer.start();
+                break;
+            case START:
+                if (sIsConnected() || cThreadIsAlive()) return;
+                else if (!sIsConnected() && cThreadIsAlive()) {
+                    cThread.interrupt();
+                    cThread = null;
+                }
+                cTimer.start();
+                break;
+            case RETRY:
+                cleanupSocket();
+                if (cThread != null) cThread.interrupt();
+                cThread = null;
+                if (shouldStop) {
+                    shouldStop = false;
+                    numOfRetries = 0;
+                    return;
+                }
+                cTimer.start();
+                break;
+            case STOP:
+                shouldStop = true;
+                break;
         }
-	}
+    }
 
-	public void informEventBus(final MessageEvent event)
-	{
-		new Thread(new Runnable()
-		{
-			@Override
-			public void run()
-			{
-				bus.post(event);
-			}
-		}).start();
-	}
+    /**
+     * Returns true if the socket is not null and it is connected, false in any other case.
+     *
+     * @return Boolean
+     */
+    private boolean sIsConnected() {
+        return clSocket != null && clSocket.isConnected();
+    }
 
-	private class socketConnection implements Runnable
-	{
-		public void run()
-		{
-			SocketAddress socketAddress = settingsManager.getSocketAddress();
-			informEventBus(new MessageEvent(SocketEvent.SocketHandshakeUpdate, false));
-			if (null == socketAddress) return;
-			BufferedReader input;
-			try
-			{
-				clSocket = new Socket();
-				clSocket.connect(socketAddress);
-				output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clSocket.getOutputStream()), 4096), true);
-				input = new BufferedReader(new InputStreamReader(clSocket.getInputStream()), 4096);
+    private void cleanupSocket() {
+        if (!sIsConnected())
+            return;
+        try {
+            if (output != null) {
+                output.flush();
+                output.close();
+                output = null;
+            }
+            clSocket.close();
+            clSocket = null;
+        } catch (IOException ignore) {
 
-				String socketStatus = String.valueOf(clSocket.isConnected());
+        }
+    }
 
-				informEventBus(new MessageEvent(SocketEvent.SocketStatusChanged, socketStatus));
-				while (clSocket.isConnected())
-				{
-					try
-					{
-						final String incoming = input.readLine();
-						if (incoming == null) throw new IOException();
+    private boolean cThreadIsAlive() {
+        return cThread != null && cThread.isAlive();
+    }
+
+    public void sendData(SocketMessage message) {
+        try {
+            if (sIsConnected())
+                output.println(mapper.writeValueAsString(message) + Const.NEWLINE);
+        } catch (Exception ignored) {
+        }
+    }
+
+    public void informEventBus(final MessageEvent event) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                bus.post(event);
+            }
+        }).start();
+    }
+
+    private class socketConnection implements Runnable {
+        public void run() {
+            SocketAddress socketAddress = settingsManager.getSocketAddress();
+            informEventBus(new MessageEvent(SocketEvent.SocketHandshakeUpdate, false));
+            if (null == socketAddress) return;
+            BufferedReader input;
+            try {
+                clSocket = new Socket();
+                clSocket.connect(socketAddress);
+                output = new PrintWriter(new BufferedWriter(new OutputStreamWriter(clSocket.getOutputStream()), 4096), true);
+                input = new BufferedReader(new InputStreamReader(clSocket.getInputStream()), 4096);
+
+                String socketStatus = String.valueOf(clSocket.isConnected());
+
+                informEventBus(new MessageEvent(SocketEvent.SocketStatusChanged, socketStatus));
+                while (clSocket.isConnected()) {
+                    try {
+                        final String incoming = input.readLine();
+                        if (incoming == null) throw new IOException();
                         if (incoming.length() > 0)
-							informEventBus(new MessageEvent(SocketEvent.SocketDataAvailable, incoming));
-					} catch (IOException e)
-					{
-						input.close();
-						clSocket.close();
-						throw e;
-					}
-				}
-			} catch (SocketTimeoutException e){
+                            informEventBus(new MessageEvent(SocketEvent.SocketDataAvailable, incoming));
+                    } catch (IOException e) {
+                        input.close();
+                        clSocket.close();
+                        throw e;
+                    }
+                }
+            } catch (SocketTimeoutException e) {
                 bus.post(new NotifyUser(R.string.notification_connection_timeout));
-			} catch (SocketException e){
+            } catch (SocketException e) {
                 bus.post(new NotifyUser(e.toString().substring(26)));
-			} catch (IOException ignored)
-			{
-			} catch (NullPointerException ignored)
-			{
-			} finally
-			{
-				if (output != null)
-				{
-					output.flush();
-					output.close();
-				}
-				clSocket = null;
+            } catch (IOException ignored) {
+            } catch (NullPointerException ignored) {
+            } finally {
+                if (output != null) {
+                    output.flush();
+                    output.close();
+                }
+                clSocket = null;
 
-				informEventBus(new MessageEvent(SocketEvent.SocketStatusChanged, false));
-				if (numOfRetries < MAX_RETRIES) SocketManager(SocketAction.RETRY);
-			}
-		}
-	}
+                informEventBus(new MessageEvent(SocketEvent.SocketStatusChanged, false));
+                if (numOfRetries < MAX_RETRIES) SocketManager(SocketAction.RETRY);
+            }
+        }
+    }
 }
