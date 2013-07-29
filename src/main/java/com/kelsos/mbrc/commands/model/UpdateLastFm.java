9b91f3eae0c46337bd801c8dd8581f3c8f3402d1
Konstantinos Paparas <kelsos86@gmail.com>
2013-07-29 21:28:40 +0300

    Fixes issue with last.fm button

--- src/main/java/com/kelsos/mbrc/commands/model/UpdateLastFm.java
+++ src/main/java/com/kelsos/mbrc/commands/model/UpdateLastFm.java
@@ -4,12 +4,13 @@ import com.google.inject.Inject;
 import com.kelsos.mbrc.interfaces.ICommand;
 import com.kelsos.mbrc.interfaces.IEvent;
 import com.kelsos.mbrc.model.MainDataModel;
+import org.codehaus.jackson.node.BooleanNode;
 
 public class UpdateLastFm implements ICommand {
     @Inject
     MainDataModel model;
     @Override
     public void execute(IEvent e) {
-        model.setScrobbleState((Boolean)e.getData());
+        model.setScrobbleState(((BooleanNode)e.getData()).asBoolean());
     }
 }
--- src/main/java/com/kelsos/mbrc/commands/model/UpdatePlayerStatus.java
+++ src/main/java/com/kelsos/mbrc/commands/model/UpdatePlayerStatus.java
@@ -8,15 +8,14 @@ import com.kelsos.mbrc.others.Protocol;
 import org.codehaus.jackson.node.ObjectNode;
 
 public class UpdatePlayerStatus implements ICommand {
-    @Inject
-    MainDataModel model;
-    @Override
-    public void execute(IEvent e) {
+    @Inject MainDataModel model;
+    @Override public void execute(IEvent e) {
         ObjectNode node = (ObjectNode)e.getData();
         model.setPlayState(node.path(Protocol.PlayerState).asText());
         model.setMuteState(node.path(Protocol.PlayerMute).asBoolean());
         model.setRepeatState(node.path(Protocol.PlayerRepeat).asText());
         model.setShuffleState(node.path(Protocol.PlayerShuffle).asBoolean());
+        model.setScrobbleState(node.path(Protocol.PlayerScrobble).asBoolean());
         model.setVolume(Integer.parseInt(node.path(Protocol.PlayerVolume).asText()));
     }
 }
