ceeed014aa98cea6240c0198922b924b19751c5b
Konstantinos Paparas <kelsos86@gmail.com>
2014-02-07 23:53:52 +0200

    Adding db items for playlist tracks

---
+++ remote/src/main/java/com/kelsos/mbrc/data/dbdata/PlaylistTrack.java
@@ -0,0 +1,26 @@
+package com.kelsos.mbrc.data.dbdata;
+
+import android.content.ContentValues;
+import com.kelsos.mbrc.data.interfaces.PlaylistTrackColumns;
+
+public class PlaylistTrack extends DataItem implements PlaylistTrackColumns {
+    @Override
+    public ContentValues getContentValues() {
+        return null;
+    }
+
+    @Override
+    public String getTableName() {
+        return null;
+    }
+
+    @Override
+    public long getId() {
+        return 0;
+    }
+
+    @Override
+    public void setId(long id) {
+
+    }
+}
---
+++ remote/src/main/java/com/kelsos/mbrc/data/interfaces/PlaylistTrackColumns.java
@@ -0,0 +1,8 @@
+package com.kelsos.mbrc.data.interfaces;
+
+public interface PlaylistTrackColumns {
+    String ARTIST = "plt_artist";
+    String TITLE = "plt_title";
+    String INDEX = "plt_index";
+    String PLAYLIST_ID = "playlist_id";
+}
