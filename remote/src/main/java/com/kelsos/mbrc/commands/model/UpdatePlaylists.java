7e0438b4af6d1085d95bb7ed4a1b578f6d8373ef
Konstantinos Paparas <kelsos86@gmail.com>
2014-02-14 23:30:03 +0200

    Building protocol for playlists

--- remote/src/main/java/com/kelsos/mbrc/commands/model/UpdatePlaylists.java
+++ remote/src/main/java/com/kelsos/mbrc/commands/model/UpdatePlaylists.java
@@ -1,9 +1,11 @@
 package com.kelsos.mbrc.commands.model;
 
+import android.content.Context;
 import android.util.Log;
 import com.google.inject.Inject;
-import com.kelsos.mbrc.data.MainDataModel;
+import com.kelsos.mbrc.data.db.LibraryDbHelper;
 import com.kelsos.mbrc.data.dbdata.NowPlayingTrack;
+import com.kelsos.mbrc.data.dbdata.Playlist;
 import com.kelsos.mbrc.interfaces.ICommand;
 import com.kelsos.mbrc.interfaces.IEvent;
 import org.codehaus.jackson.JsonNode;
@@ -13,11 +15,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class UpdatePlaylists implements ICommand {
+    private LibraryDbHelper mHelper;
 
-
-
-    @Inject public UpdatePlaylists(MainDataModel model) {
-
+    @Inject public UpdatePlaylists(final Context context) {
+        mHelper = new LibraryDbHelper(context);
     }
 
     @Override public void execute(IEvent e) {
@@ -31,7 +32,17 @@ public class UpdatePlaylists implements ICommand {
                 tracks.add(entry);
             }
             Log.d("received","total tracks:" + tracks.size());
+        } else if (jNode.path("type").asText().equals("get")) {
+            ArrayList<Playlist> playlists = new ArrayList<>();
+            ArrayNode node = (ArrayNode) jNode.path("playlists");
+            for (int i = 0; i < node.size(); i++) {
+                JsonNode cNode = node.get(i);
+                Playlist entry = new Playlist(cNode);
+                playlists.add(entry);
+            }
+            mHelper.batchInsertPlaylists(playlists);
         }
+
 
     }
 }
