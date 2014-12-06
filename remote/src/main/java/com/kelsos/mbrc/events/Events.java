3a1fb575d115c2fc29185ca16883206cb1ba660b
Konstantinos Paparas <kelsos86@gmail.com>
2014-12-06 19:51:36 +0200

    Restores functionality on MiniControlFragment

--- remote/src/main/java/com/kelsos/mbrc/events/Events.java
+++ remote/src/main/java/com/kelsos/mbrc/events/Events.java
@@ -5,12 +5,13 @@ import com.kelsos.mbrc.events.actions.ButtonPressedEvent;
 import com.kelsos.mbrc.events.ui.CoverAvailable;
 import com.kelsos.mbrc.events.ui.DiscoveryStatus;
 import com.kelsos.mbrc.events.ui.TrackInfoChange;
+import rx.subjects.BehaviorSubject;
 import rx.subjects.PublishSubject;
 
 public class Events {
     public static PublishSubject<MessageEvent> Messages = PublishSubject.create();
-    public static PublishSubject<CoverAvailable> CoverAvailableNotification = PublishSubject.create();
-    public static PublishSubject<TrackInfoChange> TrackInfoChangeNotification = PublishSubject.create();
+    public static BehaviorSubject<CoverAvailable> CoverAvailableNotification = BehaviorSubject.create();
+    public static BehaviorSubject<TrackInfoChange> TrackInfoChangeNotification = BehaviorSubject.create();
     public static PublishSubject<ConnectionSettings> ConnectionSettingsNotification = PublishSubject.create();
     public static PublishSubject<DiscoveryStatus> DiscoveryStatusNotification = PublishSubject.create();
     public static PublishSubject<ButtonPressedEvent> ButtonPressedNotification = PublishSubject.create();
--- remote/src/main/java/com/kelsos/mbrc/ui/fragments/MiniControlFragment.java
+++ remote/src/main/java/com/kelsos/mbrc/ui/fragments/MiniControlFragment.java
@@ -8,40 +8,45 @@ import android.view.ViewGroup;
 import android.widget.ImageButton;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.google.inject.Inject;
 import com.kelsos.mbrc.R;
-import com.kelsos.mbrc.events.ui.PlayStateChange;
+import com.kelsos.mbrc.data.model.PlayerState;
+import com.kelsos.mbrc.enums.PlayState;
+import com.kelsos.mbrc.events.Events;
+import com.kelsos.mbrc.events.actions.ButtonPressedEvent;
+import com.kelsos.mbrc.events.actions.ButtonPressedEvent.Button;
+import com.kelsos.mbrc.events.ui.CoverAvailable;
 import com.kelsos.mbrc.events.ui.TrackInfoChange;
+import com.kelsos.mbrc.util.Logger;
 import roboguice.fragment.provided.RoboFragment;
 import roboguice.inject.InjectView;
-
+import rx.android.observables.AndroidObservable;
+import rx.android.schedulers.AndroidSchedulers;
+import rx.schedulers.Schedulers;
 
 
 public class MiniControlFragment extends RoboFragment {
-    @InjectView (R.id.mc_track_cover) private ImageView trackCover;
-    @InjectView (R.id.mc_track_artist) private TextView trackArtist;
-    @InjectView (R.id.mc_track_title) private TextView trackTitle;
-    @InjectView (R.id.mc_next_track) private ImageButton playNext;
-    @InjectView (R.id.mc_play_pause) private ImageButton playPause;
-    @InjectView (R.id.mc_prev_track) private ImageButton playPrevious;
-    private ImageButton.OnClickListener playNextListener = new ImageButton.OnClickListener() {
 
-        @Override
-        public void onClick(View view) {
-            //getBus().post(new MessageEvent(ProtocolEventType.USER_ACTION, new UserAction(Notification.PLAYER_NEXT, true)));
-        }
-    };
-    private ImageButton.OnClickListener playPauseListener = new ImageButton.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            //getBus().post(new MessageEvent(ProtocolEventType.USER_ACTION, new UserAction(Notification.PLAYER_PLAY_PAUSE, true)));
-        }
-    };
-    private ImageButton.OnClickListener playPreviousListener = new ImageButton.OnClickListener() {
-        @Override
-        public void onClick(View view) {
-            //getBus().post(new MessageEvent(ProtocolEventType.USER_ACTION, new UserAction(Notification.PLAYER_PREVIOUS, true)));
-        }
-    };
+    @InjectView (R.id.mc_track_cover)
+    private ImageView trackCover;
+
+    @InjectView (R.id.mc_track_artist)
+    private TextView trackArtist;
+
+    @InjectView (R.id.mc_track_title)
+    private TextView trackTitle;
+
+    @InjectView (R.id.mc_next_track)
+    private ImageButton playNext;
+
+    @InjectView (R.id.mc_play_pause)
+    private ImageButton playPause;
+
+    @InjectView (R.id.mc_prev_track)
+    private ImageButton playPrevious;
+
+    @Inject
+    private PlayerState playerState;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -51,11 +56,42 @@ public class MiniControlFragment extends RoboFragment {
     @Override
     public void onStart() {
         super.onStart();
-        playNext.setOnClickListener(playNextListener);
-        playPause.setOnClickListener(playPauseListener);
-        playPrevious.setOnClickListener(playPreviousListener);
+
+        playNext.setOnClickListener(view -> Events.ButtonPressedNotification
+                .onNext(new ButtonPressedEvent(Button.NEXT)));
+
+        playPause.setOnClickListener(view -> Events.ButtonPressedNotification
+                .onNext(new ButtonPressedEvent(Button.PLAYPAUSE)));
+
+        playPrevious.setOnClickListener(view -> Events.ButtonPressedNotification
+                .onNext(new ButtonPressedEvent(Button.PREVIOUS)));
+
         Typeface robotoLight = Typeface.createFromAsset(getActivity().getAssets(), "fonts/roboto_light.ttf");
         trackTitle.setTypeface(robotoLight);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AndroidObservable.bindFragment(this, Events.CoverAvailableNotification)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::updateAlbumCover, Logger::LogThrowable);
+
+        AndroidObservable.bindFragment(this, Events.TrackInfoChangeNotification)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handleTrackInfoChange, Logger::LogThrowable);
+
+        AndroidObservable.bindFragment(this, playerState.playState())
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(this::handlePlayStateChange, Logger::LogThrowable);
+    }
+
+    private void updateAlbumCover(final CoverAvailable coverAvailable) {
+        trackCover.setImageBitmap(coverAvailable.getCover());
     }
 
     public void handleTrackInfoChange(TrackInfoChange event) {
@@ -63,8 +99,8 @@ public class MiniControlFragment extends RoboFragment {
         trackTitle.setText(event.getTitle());
     }
 
-    public void handlePlayStateChange(PlayStateChange event) {
-        switch (event.getState()) {
+    public void handlePlayStateChange(PlayState playState) {
+        switch (playState) {
             case PLAYING:
                 playPause.setImageResource(R.drawable.ic_action_pause);
                 break;
