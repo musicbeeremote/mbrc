9ee495f132953872b4bc05ae02ede1b1e1e2999b
Konstantinos Paparas <kelsos86@gmail.com>
2015-04-25 23:51:10 +0300

    Fixes shuffle backwards support for older versions of the plugin.

--- remote/src/main/java/com/kelsos/mbrc/commands/model/UpdateShuffle.java
+++ remote/src/main/java/com/kelsos/mbrc/commands/model/UpdateShuffle.java
@@ -1,9 +1,11 @@
 package com.kelsos.mbrc.commands.model;
 
 import com.google.inject.Inject;
+import com.kelsos.mbrc.events.ui.ShuffleChange;
 import com.kelsos.mbrc.interfaces.ICommand;
 import com.kelsos.mbrc.interfaces.IEvent;
 import com.kelsos.mbrc.model.MainDataModel;
+import org.codehaus.jackson.JsonNode;
 
 public class UpdateShuffle implements ICommand {
   private MainDataModel model;
@@ -13,7 +15,14 @@ public class UpdateShuffle implements ICommand {
   }
 
   @Override public void execute(IEvent e) {
+    String data = e.getDataString();
+
+    // Older plugin support, where the shuffle had boolean value.
+    if (data == null) {
+      data = ((JsonNode)e.getData()).asBoolean() ? ShuffleChange.SHUFFLE : ShuffleChange.OFF;
+    }
+
     //noinspection ResourceType
-    model.setShuffleState(e.getDataString());
+    model.setShuffleState(data);
   }
 }
