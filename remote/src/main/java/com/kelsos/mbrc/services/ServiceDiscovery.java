d2478c20841da7e044c2e4c6124d1d1e37b3e81c
Konstantinos Paparas <kelsos86@gmail.com>
2015-12-08 23:34:15 +0200

    Service discovery is now an Observable

--- remote/src/main/java/com/kelsos/mbrc/services/ServiceDiscovery.java
+++ remote/src/main/java/com/kelsos/mbrc/services/ServiceDiscovery.java
@@ -14,14 +14,15 @@ import com.kelsos.mbrc.events.ui.DiscoveryStopped;
 import com.kelsos.mbrc.utilities.MainThreadBus;
 import com.squareup.otto.Subscribe;
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
 import java.util.Hashtable;
 import java.util.Locale;
+import rx.Observable;
+import rx.schedulers.Schedulers;
 
-import static com.kelsos.mbrc.events.ui.DiscoveryStopped.*;
+import static com.kelsos.mbrc.events.ui.DiscoveryStopped.DiscoveryStop;
 
 public class ServiceDiscovery {
   public static final String NOTIFY = "notify";
@@ -29,14 +30,22 @@ public class ServiceDiscovery {
   public static final String CONTEXT = "context";
   public static final String DISCOVERY = "discovery";
   public static final String ADDRESS = "address";
+
+  public static final String DISCOVERY_ADDRESS = "239.1.5.10"; //NOPMD
+  public static final int PORT = 45345;
+  public static final int BUFFER = 512;
+
   private WifiManager manager;
   private WifiManager.MulticastLock mLock;
   private ConnectivityManager connectivityManager;
   private ObjectMapper mapper;
   private MainThreadBus bus;
 
-  @Inject public ServiceDiscovery(WifiManager manager, ConnectivityManager connectivityManager,
-      ObjectMapper mapper, MainThreadBus bus) {
+  @Inject
+  public ServiceDiscovery(WifiManager manager,
+      ConnectivityManager connectivityManager,
+      ObjectMapper mapper,
+      MainThreadBus bus) {
     this.manager = manager;
     this.connectivityManager = connectivityManager;
     this.mapper = mapper;
@@ -53,16 +62,17 @@ public class ServiceDiscovery {
   }
 
   public void startDiscovery() {
+
     if (!isWifiConnected()) {
       bus.post(new DiscoveryStopped(DiscoveryStop.NO_WIFI));
       return;
     }
-    mLock = manager.createMulticastLock("locked");
-    mLock.setReferenceCounted(true);
-    mLock.acquire();
 
-    Thread mThread = new Thread(new ServiceListener());
-    mThread.start();
+    discover().subscribeOn(Schedulers.io()).subscribe(connectionSettings -> bus.post(connectionSettings), throwable -> {
+      bus.post(new DiscoveryStopped(DiscoveryStop.COMPLETE));
+    }, () -> {
+      stopDiscovery();
+    });
   }
 
   public void stopDiscovery() {
@@ -75,8 +85,12 @@ public class ServiceDiscovery {
   private String getWifiAddress() {
     WifiInfo mInfo = manager.getConnectionInfo();
     int address = mInfo.getIpAddress();
-    return String.format(Locale.getDefault(), "%d.%d.%d.%d", (address & 0xff),
-        (address >> 8 & 0xff), (address >> 16 & 0xff), (address >> 24 & 0xff));
+    return String.format(Locale.getDefault(),
+        "%d.%d.%d.%d",
+        (address & 0xff),
+        (address >> 8 & 0xff),
+        (address >> 16 & 0xff),
+        (address >> 24 & 0xff));
   }
 
   private boolean isWifiConnected() {
@@ -84,14 +98,12 @@ public class ServiceDiscovery {
     return current != null && current.getType() == ConnectivityManager.TYPE_WIFI;
   }
 
-  private class ServiceListener implements Runnable {
-
-    public static final String DISCOVERY_ADDRESS = "239.1.5.10"; //NOPMD
-    public static final int PORT = 45345;
-    public static final int BUFFER = 512;
-
-    @Override public void run() {
+  public Observable<ConnectionSettings> discover() {
+    return Observable.create(subscriber -> {
       try {
+        mLock = manager.createMulticastLock("locked");
+        mLock.setReferenceCounted(true);
+        mLock.acquire();
 
         MulticastSocket mSocket = new MulticastSocket(45345);
         mSocket.setSoTimeout(2000);
@@ -115,21 +127,17 @@ public class ServiceDiscovery {
           JsonNode node = mapper.readValue(incoming, JsonNode.class);
           if (NOTIFY.equals(node.path(CONTEXT).asText())) {
             ConnectionSettings settings = new ConnectionSettings(node);
-            bus.post(settings);
+            subscriber.onNext(settings);
+            subscriber.onCompleted();
             break;
           }
         }
 
-        bus.post(new DiscoveryStopped(DiscoveryStop.COMPLETE));
         mSocket.leaveGroup(group);
         mSocket.close();
-        stopDiscovery();
-      } catch (InterruptedIOException e) {
-        bus.post(new DiscoveryStopped(DiscoveryStop.NOT_FOUND));
-        stopDiscovery();
       } catch (IOException e) {
-        bus.post(new DiscoveryStopped(DiscoveryStop.NOT_FOUND));
+        subscriber.onError(e);
       }
-    }
+    });
   }
 }
