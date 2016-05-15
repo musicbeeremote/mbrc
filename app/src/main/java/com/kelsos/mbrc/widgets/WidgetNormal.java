ac566f3796e3305a2e2df7225ab59c79a32d56ce
Konstantinos Paparas <kelsos86@gmail.com>
2016-05-15 22:24:55 +0300

    Adds navigation drawer to the application

--- app/src/main/java/com/kelsos/mbrc/widgets/WidgetNormal.java
+++ app/src/main/java/com/kelsos/mbrc/widgets/WidgetNormal.java
@@ -12,7 +12,7 @@ import com.kelsos.mbrc.enums.PlayState;
 import com.kelsos.mbrc.events.ui.CoverAvailable;
 import com.kelsos.mbrc.events.ui.PlayStateChange;
 import com.kelsos.mbrc.events.ui.TrackInfoChange;
-import com.kelsos.mbrc.ui.activities.MainFragmentActivity;
+import com.kelsos.mbrc.ui.activities.MainActivity;
 import com.kelsos.mbrc.utilities.RemoteViewIntentBuilder;
 import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
@@ -40,7 +40,7 @@ public class WidgetNormal extends AppWidgetProvider {
 
     for (int appWidgetId : appWidgetIds) {
       // Create an Intent to launch ExampleActivity
-      Intent intent = new Intent(context, MainFragmentActivity.class);
+      Intent intent = new Intent(context, MainActivity.class);
       PendingIntent pendingIntent = PendingIntent.getActivity(context, 0, intent, 0);
 
       // Get the layout for the App Widget and attach an on-click listener
