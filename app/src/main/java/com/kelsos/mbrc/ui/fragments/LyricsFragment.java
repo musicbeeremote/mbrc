e01d41de18c91b1e43811f22a3ff076cd1955f4d
Konstantinos Paparas <kelsos86@gmail.com>
2016-05-17 23:20:37 +0300

    Adds visual changes

--- app/src/main/java/com/kelsos/mbrc/ui/fragments/LyricsFragment.java
+++ app/src/main/java/com/kelsos/mbrc/ui/fragments/LyricsFragment.java
@@ -7,6 +7,8 @@ import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import com.google.inject.Inject;
@@ -17,12 +19,15 @@ import com.squareup.otto.Bus;
 import com.squareup.otto.Subscribe;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import roboguice.RoboGuice;
 
 public class LyricsFragment extends Fragment {
-  public static final String NEWLINE = "\r\n";
+  public static final String NEWLINE = "\r\n|\n";
   @Inject Bus bus;
   @BindView(R.id.lyrics_recycler_view) RecyclerView lyricsRecycler;
+  @BindView(R.id.empty_view) LinearLayout emptyView;
+  @BindView(R.id.empty_view_text) TextView emptyText;
 
   @Override public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
@@ -53,9 +58,20 @@ public class LyricsFragment extends Fragment {
   }
 
   @Subscribe public void updateLyricsData(LyricsUpdated update) {
-    final ArrayList<String> lyrics =
-        new ArrayList<>(Arrays.asList(update.getLyrics().split(NEWLINE)));
-    LyricsAdapter adapter = new LyricsAdapter(getActivity(), lyrics);
-    lyricsRecycler.setAdapter(adapter);
+    final String text = update.getLyrics();
+
+    final List<String> lyrics = new ArrayList<>(Arrays.asList(text.split(NEWLINE)));
+
+    if (lyrics.size() == 1) {
+      lyricsRecycler.setVisibility(View.GONE);
+      emptyText.setText(lyrics.get(0));
+      emptyView.setVisibility(View.VISIBLE);
+    } else {
+      emptyView.setVisibility(View.GONE);
+      lyricsRecycler.setVisibility(View.VISIBLE);
+      LyricsAdapter adapter = new LyricsAdapter(getActivity(), lyrics);
+      lyricsRecycler.setAdapter(adapter);
+    }
+
   }
 }
