b533037291b9130bc0694e1c572c205ad941ac40
Konstantinos Paparas <kelsos86@gmail.com>
2016-05-25 00:21:08 +0300

    Adds album artist to track for sorting Adds DiscoveryMessage to parse directly, and Rxifies service discovery

--- app/src/main/java/com/kelsos/mbrc/services/ServiceDiscovery.java
+++ app/src/main/java/com/kelsos/mbrc/services/ServiceDiscovery.java
@@ -4,33 +4,45 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
-import com.fasterxml.jackson.databind.JsonNode;
+import android.support.annotation.NonNull;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.inject.Inject;
 import com.kelsos.mbrc.constants.Const;
 import com.kelsos.mbrc.constants.Protocol;
 import com.kelsos.mbrc.data.ConnectionSettings;
+import com.kelsos.mbrc.data.DiscoveryMessage;
 import com.kelsos.mbrc.enums.DiscoveryStop;
 import com.kelsos.mbrc.events.ui.DiscoveryStopped;
 import com.kelsos.mbrc.utilities.MainThreadBusWrapper;
 import java.io.IOException;
-import java.io.InterruptedIOException;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.net.MulticastSocket;
-import java.util.Hashtable;
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+import rx.Observable;
+import rx.Subscriber;
+import rx.schedulers.Schedulers;
+import timber.log.Timber;
 
 public class ServiceDiscovery {
   public static final String NOTIFY = "notify";
+  public static final int SO_TIMEOUT = 6 * 1000;
+  public static final int MULTICASTPORT = 45345;
+  public static final String DISCOVERY_ADDRESS = "239.1.5.10"; //NOPMD
+
   private WifiManager manager;
   private WifiManager.MulticastLock mLock;
   private ConnectivityManager connectivityManager;
   private ObjectMapper mapper;
   private MainThreadBusWrapper bus;
+  private InetAddress group;
 
-  @Inject public ServiceDiscovery(WifiManager manager, ConnectivityManager connectivityManager,
-      ObjectMapper mapper, MainThreadBusWrapper bus) {
+  @Inject
+  public ServiceDiscovery(WifiManager manager,
+      ConnectivityManager connectivityManager,
+      ObjectMapper mapper,
+      MainThreadBusWrapper bus) {
     this.manager = manager;
     this.connectivityManager = connectivityManager;
     this.mapper = mapper;
@@ -46,8 +58,17 @@ public class ServiceDiscovery {
     mLock.setReferenceCounted(true);
     mLock.acquire();
 
-    Thread mThread = new Thread(new ServiceListener());
-    mThread.start();
+    discoveryObservable().subscribeOn(Schedulers.io())
+        .unsubscribeOn(Schedulers.io())
+        .doOnTerminate(this::stopDiscovery)
+        .subscribe(message -> {
+          Timber.v("Incoming discovery message -> %s", message);
+          ConnectionSettings settings = new ConnectionSettings(message);
+          bus.post(settings);
+          bus.post(new DiscoveryStopped(DiscoveryStop.COMPLETE));
+        }, throwable -> {
+          bus.post(new DiscoveryStopped(DiscoveryStop.NOT_FOUND));
+        });
   }
 
   public void stopDiscovery() {
@@ -60,8 +81,12 @@ public class ServiceDiscovery {
   private String getWifiAddress() {
     WifiInfo mInfo = manager.getConnectionInfo();
     int address = mInfo.getIpAddress();
-    return String.format(Locale.getDefault(), "%d.%d.%d.%d", (address & 0xff),
-        (address >> 8 & 0xff), (address >> 16 & 0xff), (address >> 24 & 0xff));
+    return String.format(Locale.getDefault(),
+        "%d.%d.%d.%d",
+        (address & 0xff),
+        (address >> 8 & 0xff),
+        (address >> 16 & 0xff),
+        (address >> 24 & 0xff));
   }
 
   private boolean isWifiConnected() {
@@ -69,50 +94,57 @@ public class ServiceDiscovery {
     return current != null && current.getType() == ConnectivityManager.TYPE_WIFI;
   }
 
-  private class ServiceListener implements Runnable {
+  @NonNull
+  private Observable<DiscoveryMessage> discoveryObservable() {
+    return Observable.using(this::getResource, this::getObservable, this::cleanup);
+  }
 
-    public static final String DISCOVERY_ADDRESS = "239.1.5.10"; //NOPMD
+  private void cleanup(MulticastSocket resource) {
+    try {
+      resource.leaveGroup(group);
+      resource.close();
+      stopDiscovery();
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
-    @Override public void run() {
-      try {
-
-        MulticastSocket mSocket = new MulticastSocket(45345);
-        mSocket.setSoTimeout(2000);
-        InetAddress group = InetAddress.getByName(DISCOVERY_ADDRESS);
-        mSocket.joinGroup(group);
-
-        DatagramPacket mPacket;
-        byte[] buffer = new byte[512];
-        mPacket = new DatagramPacket(buffer, buffer.length);
-        Hashtable<String, String> discoveryMessage = new Hashtable<>();
-        discoveryMessage.put(Protocol.CONTEXT, Protocol.DISCOVERY);
-        discoveryMessage.put(Protocol.ADDRESS, getWifiAddress());
-        byte[] discovery = mapper.writeValueAsBytes(discoveryMessage);
-        mSocket.send(new DatagramPacket(discovery, discovery.length, group, 45345));
-        String incoming;
-
-        while (true) {
-          mSocket.receive(mPacket);
-          incoming = new String(mPacket.getData(), Const.UTF_8);
-
-          JsonNode node = mapper.readValue(incoming, JsonNode.class);
-          if (NOTIFY.equals(node.path(Protocol.CONTEXT).asText())) {
-            ConnectionSettings settings = new ConnectionSettings(node);
-            bus.post(settings);
-            break;
+  @NonNull
+  private Observable<DiscoveryMessage> getObservable(MulticastSocket socket) {
+    return Observable.interval(600, TimeUnit.MILLISECONDS)
+        .take(6)
+        .flatMap(aLong -> Observable.create((Subscriber<? super DiscoveryMessage> subscriber) -> {
+          try {
+            DatagramPacket mPacket;
+            byte[] buffer = new byte[512];
+            mPacket = new DatagramPacket(buffer, buffer.length);
+            socket.receive(mPacket);
+            String incoming = new String(mPacket.getData(), Const.UTF_8);
+            DiscoveryMessage node = mapper.readValue(incoming, DiscoveryMessage.class);
+            subscriber.onNext(node);
+            subscriber.onCompleted();
+          } catch (IOException e) {
+            subscriber.onError(e);
           }
-        }
+        }))
+        .filter(message -> NOTIFY.equals(message.getContext()))
+        .first();
+  }
 
-        bus.post(new DiscoveryStopped(DiscoveryStop.COMPLETE));
-        mSocket.leaveGroup(group);
-        mSocket.close();
-        stopDiscovery();
-      } catch (InterruptedIOException e) {
-        bus.post(new DiscoveryStopped(DiscoveryStop.NOT_FOUND));
-        stopDiscovery();
-      } catch (IOException e) {
-        bus.post(new DiscoveryStopped(DiscoveryStop.NOT_FOUND));
-      }
+  private MulticastSocket getResource() {
+    try {
+      MulticastSocket multicastSocket = new MulticastSocket(MULTICASTPORT);
+      multicastSocket.setSoTimeout(SO_TIMEOUT);
+      group = InetAddress.getByName(DISCOVERY_ADDRESS);
+      multicastSocket.joinGroup(group);
+      DiscoveryMessage message = new DiscoveryMessage();
+      message.setContext(Protocol.DISCOVERY);
+      message.setAddress(getWifiAddress());
+      byte[] discovery = mapper.writeValueAsBytes(message);
+      multicastSocket.send(new DatagramPacket(discovery, discovery.length, group, MULTICASTPORT));
+      return multicastSocket;
+    } catch (IOException e) {
+      throw new RuntimeException(e);
     }
   }
 }
