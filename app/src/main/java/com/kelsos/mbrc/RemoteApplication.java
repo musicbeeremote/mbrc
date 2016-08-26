6c751e33706b9dd858b4d420d526dc00061049b9
Konstantinos Paparas <kelsos86@gmail.com>
2016-08-27 00:37:17 +0300

    Updates some dependencies

--- app/src/main/java/com/kelsos/mbrc/RemoteApplication.java
+++ app/src/main/java/com/kelsos/mbrc/RemoteApplication.java
@@ -1,25 +1,36 @@
 package com.kelsos.mbrc;
 
 import android.app.Application;
-import com.kelsos.mbrc.di.modules.ApplicationModule;
+
 import com.kelsos.mbrc.di.modules.RemoteModule;
+
 import timber.log.Timber;
-import toothpick.Configuration;
 import toothpick.Scope;
 import toothpick.Toothpick;
+import toothpick.configuration.Configuration;
 import toothpick.registries.FactoryRegistryLocator;
 import toothpick.registries.MemberInjectorRegistryLocator;
+import toothpick.smoothie.module.SmoothieApplicationModule;
 import uk.co.chrisjenx.calligraphy.CalligraphyConfig;
 
 public class RemoteApplication extends Application {
 
   public void onCreate() {
     super.onCreate();
-    Configuration.setConfiguration(Configuration.reflectionFree());
+
+    Configuration configuration;
+    if (BuildConfig.DEBUG) {
+      configuration = Configuration.forDevelopment().disableReflection();
+    } else {
+      configuration = Configuration.forProduction().disableReflection();
+    }
+
+    Toothpick.setConfiguration(configuration);
+
     MemberInjectorRegistryLocator.setRootRegistry(new MemberInjectorRegistry());
     FactoryRegistryLocator.setRootRegistry(new FactoryRegistry());
     Scope applicationScope = Toothpick.openScope(this);
-    applicationScope.installModules(new ApplicationModule(this), new RemoteModule());
+    applicationScope.installModules(new SmoothieApplicationModule(this), new RemoteModule());
 
     CalligraphyConfig.initDefault(new CalligraphyConfig.Builder()
         .setDefaultFontPath("fonts/roboto_regular.ttf")
