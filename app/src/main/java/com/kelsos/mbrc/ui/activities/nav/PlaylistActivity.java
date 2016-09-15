21b618730294d7d0a5ffd7b68d1bf01d98aa9912
Konstantinos Paparas <kelsos86@gmail.com>
2016-09-15 19:21:06 +0300

    Adds swipe to refresh functionality on playlist view

--- app/src/main/java/com/kelsos/mbrc/ui/activities/nav/PlaylistActivity.java
+++ app/src/main/java/com/kelsos/mbrc/ui/activities/nav/PlaylistActivity.java
@@ -1,10 +1,11 @@
 package com.kelsos.mbrc.ui.activities.nav;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-
-import javax.inject.Inject;
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import com.kelsos.mbrc.R;
 import com.kelsos.mbrc.adapters.PlaylistAdapter;
 import com.kelsos.mbrc.constants.Protocol;
@@ -14,14 +15,14 @@ import com.kelsos.mbrc.events.MessageEvent;
 import com.kelsos.mbrc.events.bus.RxBus;
 import com.kelsos.mbrc.events.ui.PlaylistAvailable;
 import com.kelsos.mbrc.ui.activities.BaseActivity;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
+import javax.inject.Inject;
 import toothpick.Scope;
 import toothpick.Toothpick;
 
-public class PlaylistActivity extends BaseActivity implements PlaylistAdapter.OnPlaylistPressedListener {
+public class PlaylistActivity extends BaseActivity implements PlaylistAdapter.OnPlaylistPressedListener,
+    SwipeRefreshLayout.OnRefreshListener {
 
+  @BindView(R.id.swipe_layout) SwipeRefreshLayout swipeLayout;
   @BindView(R.id.playlist_list) RecyclerView playlistList;
 
   @Inject PlaylistAdapter adapter;
@@ -32,7 +33,8 @@ public class PlaylistActivity extends BaseActivity implements PlaylistAdapter.On
     // Required empty public constructor
   }
 
-  @Override public void onCreate(Bundle savedInstanceState) {
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
     scope = Toothpick.openScopes(getApplication(), this);
     super.onCreate(savedInstanceState);
     Toothpick.inject(this, scope);
@@ -42,24 +44,29 @@ public class PlaylistActivity extends BaseActivity implements PlaylistAdapter.On
     adapter.setPlaylistPressedListener(this);
     playlistList.setAdapter(adapter);
     playlistList.setLayoutManager(new LinearLayoutManager(this));
+    swipeLayout.setOnRefreshListener(this);
   }
 
-  @Override public void onStart() {
+  @Override
+  public void onStart() {
     super.onStart();
     bus.register(this, PlaylistAvailable.class, this::onPlaylistAvailable, true);
     bus.post(new MessageEvent(ProtocolEventType.UserAction, new UserAction(Protocol.PlaylistList, true)));
   }
 
-  @Override public void onStop() {
+  @Override
+  public void onStop() {
     super.onStop();
     bus.unregister(this);
   }
 
   private void onPlaylistAvailable(PlaylistAvailable event) {
+    swipeLayout.setRefreshing(false);
     adapter.update(event.getPlaylist());
   }
 
-  @Override public void playlistPressed(String path) {
+  @Override
+  public void playlistPressed(String path) {
     bus.post(new MessageEvent(ProtocolEventType.UserAction, new UserAction(Protocol.PlaylistPlay, path)));
   }
 
@@ -72,5 +79,13 @@ public class PlaylistActivity extends BaseActivity implements PlaylistAdapter.On
   protected void onDestroy() {
     Toothpick.closeScope(this);
     super.onDestroy();
+  }
+
+  @Override
+  public void onRefresh() {
+    if (!swipeLayout.isRefreshing()) {
+      swipeLayout.setRefreshing(true);
+    }
+    bus.post(new MessageEvent(ProtocolEventType.UserAction, new UserAction(Protocol.PlaylistList, true)));
   }
 }
