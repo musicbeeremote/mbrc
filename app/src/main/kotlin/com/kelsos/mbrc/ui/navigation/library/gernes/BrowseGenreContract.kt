72081ba06b9ce03b28c5972c66ed99c95ceb7850
Konstantinos Paparas <kelsos86@gmail.com>
2016-10-28 00:12:41 +0300

    Restoring swipe functionality for BrowseGenre

---
+++ app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/gernes/BrowseGenreContract.kt
@@ -0,0 +1,16 @@
+package com.kelsos.mbrc.ui.navigation.library.gernes
+
+import com.kelsos.mbrc.data.library.Genre
+import com.kelsos.mbrc.mvp.BaseView
+import com.kelsos.mbrc.mvp.Presenter
+import com.raizlabs.android.dbflow.list.FlowCursorList
+
+interface BrowseGenrePresenter : Presenter<BrowseGenreView> {
+  fun load()
+  fun reload()
+}
+
+interface BrowseGenreView : BaseView {
+  fun update(cursor: FlowCursorList<Genre>)
+  fun failure(it: Throwable)
+}
--- app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/gernes/BrowseGenreFragment.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/gernes/BrowseGenreFragment.kt
@@ -1,8 +1,9 @@
 package com.kelsos.mbrc.ui.navigation.library.gernes
 
 import android.os.Bundle
+import android.support.design.widget.Snackbar
 import android.support.v4.app.Fragment
-import android.support.v4.widget.SwipeRefreshLayout
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener
 import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.MenuItem
@@ -13,21 +14,22 @@ import butterknife.BindView
 import butterknife.ButterKnife
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.adapters.GenreEntryAdapter
+import com.kelsos.mbrc.adapters.GenreEntryAdapter.MenuItemSelectedListener
 import com.kelsos.mbrc.data.library.Genre
 import com.kelsos.mbrc.events.bus.RxBus
-import com.kelsos.mbrc.events.ui.NotifyUser
 import com.kelsos.mbrc.helper.PopupActionHandler
 import com.kelsos.mbrc.services.BrowseSync
 import com.kelsos.mbrc.ui.widgets.EmptyRecyclerView
 import com.kelsos.mbrc.ui.widgets.MultiSwipeRefreshLayout
-import rx.Subscription
-import rx.android.schedulers.AndroidSchedulers
-import rx.schedulers.Schedulers
-import timber.log.Timber
+import com.raizlabs.android.dbflow.list.FlowCursorList
 import toothpick.Toothpick
 import javax.inject.Inject
 
-class BrowseGenreFragment : Fragment(), GenreEntryAdapter.MenuItemSelectedListener, SwipeRefreshLayout.OnRefreshListener {
+class BrowseGenreFragment : Fragment(),
+    BrowseGenreView,
+    MenuItemSelectedListener,
+    OnRefreshListener {
+
   @BindView(R.id.library_data_list) lateinit var recycler: EmptyRecyclerView
   @BindView(R.id.empty_view) lateinit var emptyView: View
   @BindView(R.id.swipe_layout) lateinit var swipeLayout: MultiSwipeRefreshLayout
@@ -37,9 +39,11 @@ class BrowseGenreFragment : Fragment(), GenreEntryAdapter.MenuItemSelectedListen
   @Inject lateinit var bus: RxBus
   @Inject lateinit var actionHandler: PopupActionHandler
   @Inject lateinit var sync: BrowseSync
-  private var subscription: Subscription? = null
+  @Inject lateinit var presenter: BrowseGenrePresenter
 
-  override fun onCreateView(inflater: LayoutInflater?, container: ViewGroup?, savedInstanceState: Bundle?): View? {
+  override fun onCreateView(inflater: LayoutInflater?,
+                            container: ViewGroup?,
+                            savedInstanceState: Bundle?): View? {
     val view = inflater!!.inflate(R.layout.fragment_library_search, container, false)
     ButterKnife.bind(this, view)
     swipeLayout.setSwipeableChildren(R.id.library_data_list, R.id.empty_view)
@@ -48,15 +52,27 @@ class BrowseGenreFragment : Fragment(), GenreEntryAdapter.MenuItemSelectedListen
   }
 
   override fun onCreate(savedInstanceState: Bundle?) {
-    val activity = activity
-    val scope = Toothpick.openScopes(activity.application, activity, this)
     super.onCreate(savedInstanceState)
+    val scope = Toothpick.openScopes(activity.application, activity, this)
+    scope.installModules(BrowseGenreModule())
     Toothpick.inject(this, scope)
+    presenter.attach(this)
+    presenter.load()
   }
 
   override fun onStart() {
     super.onStart()
-    adapter.init()
+    presenter.attach(this)
+  }
+
+  override fun onStop() {
+    super.onStop()
+    presenter.detach()
+  }
+
+  override fun update(cursor: FlowCursorList<Genre>) {
+    swipeLayout.isRefreshing = false
+    adapter.update(cursor)
   }
 
   override fun onViewCreated(view: View?, savedInstanceState: Bundle?) {
@@ -84,17 +100,13 @@ class BrowseGenreFragment : Fragment(), GenreEntryAdapter.MenuItemSelectedListen
       swipeLayout.isRefreshing = true
     }
 
-    if (subscription != null && !subscription!!.isUnsubscribed) {
-      return
-    }
+    presenter.reload()
 
-    subscription = sync.syncGenres(Schedulers.io())
-        .observeOn(AndroidSchedulers.mainThread())
-        .doOnTerminate { swipeLayout.isRefreshing = false }
-        .subscribe({ adapter.refresh() }) {
-          bus.post(NotifyUser(R.string.refresh_failed))
-          Timber.v(it, "Refresh failed")
-        }
+  }
+
+  override fun failure(it: Throwable) {
+    swipeLayout.isRefreshing = false
+   Snackbar.make(recycler, R.string.refresh_failed, Snackbar.LENGTH_SHORT).show()
   }
 
   override fun onDestroy() {
