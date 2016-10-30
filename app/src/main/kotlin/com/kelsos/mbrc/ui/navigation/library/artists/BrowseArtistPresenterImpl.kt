067feda1aa7d98d5295b5664f6784263bcb9deb9
Konstantinos Paparas <kelsos86@gmail.com>
2016-10-30 15:03:41 +0200

    Fixes reloading for albums and artists

--- app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/artists/BrowseArtistPresenterImpl.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/library/artists/BrowseArtistPresenterImpl.kt
@@ -1,16 +1,23 @@
 package com.kelsos.mbrc.ui.navigation.library.artists
 
+import com.kelsos.mbrc.data.library.Artist
 import com.kelsos.mbrc.mvp.BasePresenter
 import com.kelsos.mbrc.repository.ArtistRepository
+import com.raizlabs.android.dbflow.list.FlowCursorList
+import rx.Scheduler
+import rx.Single
 import timber.log.Timber
 import javax.inject.Inject
+import javax.inject.Named
 
 class BrowseArtistPresenterImpl
-@Inject constructor(private val repository: ArtistRepository) :
+@Inject constructor(private val repository: ArtistRepository,
+                    @Named("io") private val ioScheduler: Scheduler,
+                    @Named("main") private val mainScheduler: Scheduler) :
     BasePresenter<BrowseArtistView>(),
     BrowseArtistPresenter {
   override fun load() {
-    addSubcription(repository.getAllCursor().subscribe({
+    addSubcription(repository.getAllCursor().compose  { schedule(it) }.subscribe({
       view?.update(it)
     }, {
       Timber.v(it, "Error while loading the data from the database")
@@ -19,11 +26,14 @@ class BrowseArtistPresenterImpl
   }
 
   override fun reload() {
-    addSubcription(repository.getAndSaveRemote().subscribe({
+    addSubcription(repository.getAndSaveRemote().compose  { schedule(it) }.subscribe({
       view?.update(it)
     }, {
       Timber.v(it, "Error retrieving the data")
     }))
   }
+
+  private fun schedule(it: Single<FlowCursorList<Artist>>) = it.observeOn(mainScheduler)
+      .subscribeOn(ioScheduler)
 
 }
