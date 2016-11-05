ee20cb86a5b4ad87b8d6944c6e63d2422ef700a2
Konstantinos Paparas <kelsos86@gmail.com>
2016-11-06 01:25:39 +0200

    Fixes auto reload mini control and notification

--- app/src/main/kotlin/com/kelsos/mbrc/commands/model/UpdatePlayState.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/commands/model/UpdatePlayState.kt
@@ -1,14 +1,18 @@
 package com.kelsos.mbrc.commands.model
 
+import android.app.Application
 import com.kelsos.mbrc.interfaces.ICommand
 import com.kelsos.mbrc.interfaces.IEvent
 import com.kelsos.mbrc.model.MainDataModel
+import com.kelsos.mbrc.widgets.UpdateWidgets
 import javax.inject.Inject
 
 class UpdatePlayState
-@Inject constructor(private val model: MainDataModel) : ICommand {
+@Inject constructor(private val model: MainDataModel,
+                    private val context: Application) : ICommand {
 
   override fun execute(e: IEvent) {
     model.playState = e.dataString
+    UpdateWidgets.update(context)
   }
 }
--- app/src/main/kotlin/com/kelsos/mbrc/ui/mini_control/MiniControlPresenterImpl.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/ui/mini_control/MiniControlPresenterImpl.kt
@@ -1,22 +1,37 @@
 package com.kelsos.mbrc.ui.mini_control
 
-import com.kelsos.mbrc.constants.Const
+import com.kelsos.mbrc.events.bus.RxBus
+import com.kelsos.mbrc.events.ui.CoverChangedEvent
+import com.kelsos.mbrc.events.ui.PlayStateChange
+import com.kelsos.mbrc.events.ui.TrackInfoChangeEvent
 import com.kelsos.mbrc.model.MainDataModel
 import com.kelsos.mbrc.mvp.BasePresenter
 import javax.inject.Inject
 
-class MiniControlPresenterImpl : BasePresenter<MiniControlView>(), MiniControlPresenter {
-
-  @Inject lateinit var model: MainDataModel
+class MiniControlPresenterImpl
+@Inject constructor(private val model: MainDataModel,
+                    private val bus: RxBus) :
+    BasePresenter<MiniControlView>(), MiniControlPresenter {
 
   override fun load() {
     if (!isAttached) {
       return
     }
-
-    view?.updateCover(Const.COVER_FILE)
+    view?.updateCover()
     view?.updateState(model.playState)
     view?.updateTrackInfo(model.trackInfo)
+  }
+
+  override fun attach(view: MiniControlView) {
+    super.attach(view)
+    bus.register(this, CoverChangedEvent::class.java, { this.view?.updateCover() }, true)
+    bus.register(this, TrackInfoChangeEvent::class.java, { this.view?.updateTrackInfo(it.trackInfo) }, true)
+    bus.register(this, PlayStateChange::class.java, { this.view?.updateState(it.state) }, true)
+  }
+
+  override fun detach() {
+    super.detach()
+    bus.unregister(this)
   }
 
 }
