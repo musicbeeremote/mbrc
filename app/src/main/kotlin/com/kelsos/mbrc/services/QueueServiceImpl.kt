e7a6c0aaf8972014ec41fd88b83688eb364e1790
Konstantinos Paparas <kelsos86@gmail.com>
2016-11-16 23:07:54 +0200

    #85 Reworking the queue on the client side

---
+++ app/src/main/kotlin/com/kelsos/mbrc/services/QueueServiceImpl.kt
@@ -0,0 +1,24 @@
+package com.kelsos.mbrc.services
+
+import com.kelsos.mbrc.constants.Protocol
+import com.kelsos.mbrc.data.CoverPayload
+import com.kelsos.mbrc.data.QueuePayload
+import com.kelsos.mbrc.data.QueueResponse
+import rx.Single
+import javax.inject.Inject
+
+class QueueServiceImpl
+@Inject constructor() : ServiceBase(), QueueService {
+  override fun queue(type: String, tracks: List<String>): Single<QueueResponse> {
+    return request(Protocol.NowPlayingQueue, QueuePayload(type, tracks)).toSingle().flatMap {
+      return@flatMap Single.fromCallable {
+        val payload = mapper.readValue<QueueResponse>(it.data as String, QueueResponse::class.java)
+        if (payload.code == CoverPayload.SUCCESS) {
+          return@fromCallable payload
+        } else {
+          throw RuntimeException("Queueing of tracks failed")
+        }
+      }
+    }
+  }
+}
