3279ffe47e2ec848cc46e2e4ce521fc88375bc49
Konstantinos Paparas <kelsos86@gmail.com>
2016-11-19 21:17:56 +0200

    Fixes an issue with the persistent track info not getting restored properly

--- app/src/main/kotlin/com/kelsos/mbrc/model/MainDataModel.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/model/MainDataModel.kt
@@ -23,132 +23,136 @@ class MainDataModel
 @Inject
 constructor(private val bus: RxBus,
             private val cache: TrackCache) {
+  private var _trackInfo: TrackInfo = TrackInfo()
 
-    init {
-        cache.restore().subscribe({
-            trackInfo = it
-        }, {
-            Timber.v(it, "No state was previously stored")
-        })
+  init {
+    cache.restore().subscribe({
+      trackInfo = it
+    }, {
+      Timber.v(it, "No state was previously stored")
+    })
+  }
+
+  var rating: Float = 0f
+    set(value) {
+      field = value
+      bus.post(RatingChanged(field))
     }
 
-    var rating: Float = 0f
-        set(value) {
-            field = value
-            bus.post(RatingChanged(field))
-        }
-
-    var volume: Int = 0
-        get
-        set(value) {
-            if (value != field) {
-                field = value
-                bus.post(VolumeChange(field))
-            }
-        }
-
-    @ShuffleState var shuffle: String = ShuffleChange.OFF
-        set(value) {
-            field = value
-            bus.post(ShuffleChange(value))
-        }
-
-    var isScrobblingEnabled: Boolean = false
-        set(value) {
-            field = value
-            bus.post(ScrobbleChange(field))
-        }
-
-    var isMute: Boolean = false
-        set(value) {
-            field = value
-            bus.post(if (value) VolumeChange() else VolumeChange(volume))
-        }
-
-
-    var lfmStatus: LfmStatus = LfmStatus.NORMAL
-        private set
-
-    var pluginVersion: String = "1.0.0"
-        set(value) {
-            if (value.isNullOrEmpty()) {
-                return
-            }
-            field = value.substring(0, value.lastIndexOf('.'))
-            bus.post(MessageEvent(ProtocolEventType.PluginVersionCheck))
-        }
-
-    var pluginProtocol: Int = 2
-
-    @State var playState: String = PlayerState.UNDEFINED
-        set(value) {
-            @State val newState: String
-            if (Const.PLAYING.equals(value, ignoreCase = true)) {
-                newState = PlayerState.PLAYING
-            } else if (Const.STOPPED.equals(value, ignoreCase = true)) {
-                newState = PlayerState.STOPPED
-            } else if (Const.PAUSED.equals(value, ignoreCase = true)) {
-                newState = PlayerState.PAUSED
-            } else {
-                newState = PlayerState.UNDEFINED
-            }
-
-            field = newState
-
-            bus.post(PlayStateChange(field))
-        }
-
-    @Mode
-    var repeat: String
-        private set
-
-    init {
-        repeat = Repeat.NONE
-        rating = 0f
-
-        lfmStatus = LfmStatus.NORMAL
-        pluginVersion = Const.EMPTY
+  var volume: Int = 0
+    get
+    set(value) {
+      if (value != field) {
+        field = value
+        bus.post(VolumeChange(field))
+      }
     }
 
-    fun setLfmRating(rating: String) {
-        when (rating) {
-            "Love" -> lfmStatus = LfmStatus.LOVED
-            "Ban" -> lfmStatus = LfmStatus.BANNED
-            else -> lfmStatus = LfmStatus.NORMAL
-        }
-
-        bus.post(LfmRatingChanged(lfmStatus))
+  @ShuffleState var shuffle: String = ShuffleChange.OFF
+    set(value) {
+      field = value
+      bus.post(ShuffleChange(value))
     }
 
-    fun updateRemoteClient() {
-        bus.post(RemoteClientMetaData(trackInfo))
+  var isScrobblingEnabled: Boolean = false
+    set(value) {
+      field = value
+      bus.post(ScrobbleChange(field))
     }
 
-    fun setRepeatState(repeat: String) {
-        if (Protocol.ALL.equals(repeat, ignoreCase = true)) {
-            this.repeat = Repeat.ALL
-        } else if (Protocol.ONE.equals(repeat, ignoreCase = true)) {
-            this.repeat = Repeat.ONE
-        } else {
-            this.repeat = Repeat.NONE
-        }
-
-        bus.post(RepeatChange(this.repeat))
+  var isMute: Boolean = false
+    set(value) {
+      field = value
+      bus.post(if (value) VolumeChange() else VolumeChange(volume))
     }
 
-    var trackInfo: TrackInfo = TrackInfo()
-        get
-        set(value) {
-            field = value
-            val event = TrackInfoChangeEvent(value)
-            bus.post(event)
-            updateRemoteClient()
-            cache.persist(value).subscribe({
-                Timber.v("Playing track info successfully persisted")
-            }) {
-                Timber.v(it, "Failed to perist the playing track info")
-            }
-        }
+
+  var lfmStatus: LfmStatus = LfmStatus.NORMAL
+    private set
+
+  var pluginVersion: String = "1.0.0"
+    set(value) {
+      if (value.isNullOrEmpty()) {
+        return
+      }
+      field = value.substring(0, value.lastIndexOf('.'))
+      bus.post(MessageEvent(ProtocolEventType.PluginVersionCheck))
+    }
+
+  var pluginProtocol: Int = 2
+
+  @State var playState: String = PlayerState.UNDEFINED
+    set(value) {
+      @State val newState: String
+      if (Const.PLAYING.equals(value, ignoreCase = true)) {
+        newState = PlayerState.PLAYING
+      } else if (Const.STOPPED.equals(value, ignoreCase = true)) {
+        newState = PlayerState.STOPPED
+      } else if (Const.PAUSED.equals(value, ignoreCase = true)) {
+        newState = PlayerState.PAUSED
+      } else {
+        newState = PlayerState.UNDEFINED
+      }
+
+      field = newState
+
+      bus.post(PlayStateChange(field))
+    }
+
+  @Mode
+  var repeat: String
+    private set
+
+  init {
+    repeat = Repeat.NONE
+    rating = 0f
+
+    lfmStatus = LfmStatus.NORMAL
+    pluginVersion = Const.EMPTY
+  }
+
+  fun setLfmRating(rating: String) {
+    when (rating) {
+      "Love" -> lfmStatus = LfmStatus.LOVED
+      "Ban" -> lfmStatus = LfmStatus.BANNED
+      else -> lfmStatus = LfmStatus.NORMAL
+    }
+
+    bus.post(LfmRatingChanged(lfmStatus))
+  }
+
+  fun updateRemoteClient() {
+    bus.post(RemoteClientMetaData(trackInfo))
+  }
+
+  fun setRepeatState(repeat: String) {
+    if (Protocol.ALL.equals(repeat, ignoreCase = true)) {
+      this.repeat = Repeat.ALL
+    } else if (Protocol.ONE.equals(repeat, ignoreCase = true)) {
+      this.repeat = Repeat.ONE
+    } else {
+      this.repeat = Repeat.NONE
+    }
+
+    bus.post(RepeatChange(this.repeat))
+  }
+
+
+  var trackInfo: TrackInfo
+    get() {
+      return _trackInfo
+    }
+    set(value) {
+      _trackInfo = value
+      val event = TrackInfoChangeEvent(value)
+      bus.post(event)
+      updateRemoteClient()
+      cache.persist(value).subscribe({
+        Timber.v("Playing track info successfully persisted")
+      }) {
+        Timber.v(it, "Failed to perist the playing track info")
+      }
+    }
 
 }
 
