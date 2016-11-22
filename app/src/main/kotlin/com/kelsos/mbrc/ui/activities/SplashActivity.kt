87aa9b39c39f0504c1914a60ef238f9be8dc05c0
Konstantinos Paparas <kelsos86@gmail.com>
2016-11-22 22:14:38 +0200

    Adds splash screen

---
+++ app/src/main/kotlin/com/kelsos/mbrc/ui/activities/SplashActivity.kt
@@ -0,0 +1,25 @@
+package com.kelsos.mbrc.ui.activities
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import android.support.v7.app.AppCompatActivity
+import com.kelsos.mbrc.R
+import com.kelsos.mbrc.ui.navigation.main.MainActivity
+import toothpick.Toothpick
+
+class SplashActivity : AppCompatActivity() {
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_splash)
+    Toothpick.openScope(application)
+    val handler = Handler(Looper.getMainLooper())
+
+    handler.postDelayed({
+      val intent = Intent(this, MainActivity::class.java)
+      startActivity(intent)
+    }, 1500)
+  }
+}
