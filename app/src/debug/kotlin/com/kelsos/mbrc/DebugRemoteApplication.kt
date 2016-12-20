4d666e8850155a8fb5380f2de2dc33c7dfa2fd53
Konstantinos Paparas <kelsos86@gmail.com>
2016-12-20 17:57:54 +0200

    Updates version, removes custom configuration for leak canary (the configuration would fail all the tests)

--- app/src/debug/kotlin/com/kelsos/mbrc/DebugRemoteApplication.kt
+++ app/src/debug/kotlin/com/kelsos/mbrc/DebugRemoteApplication.kt
@@ -2,13 +2,9 @@ package com.kelsos.mbrc
 
 import com.squareup.leakcanary.LeakCanary
 import com.squareup.leakcanary.RefWatcher
-import java.util.concurrent.TimeUnit
 
 class DebugRemoteApplication : RemoteApplication() {
   override fun installLeakCanary(): RefWatcher {
-    return LeakCanary.refWatcher(this)
-        .watchDelay(10, TimeUnit.SECONDS)
-        .buildAndInstall()
+    return LeakCanary.install(this)
   }
-
 }
