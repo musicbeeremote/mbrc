0448763bf648481196a2a5c8791806c409eec0dc
Konstantinos Paparas <kelsos86@gmail.com>
2017-01-09 22:46:20 +0200

    Adds test buttler

--- app/src/androidTest/kotlin/com/kelsos/mbrc/MockTestRunner.kt
+++ app/src/androidTest/kotlin/com/kelsos/mbrc/MockTestRunner.kt
@@ -2,9 +2,23 @@ package com.kelsos.mbrc
 
 import android.app.Application
 import android.content.Context
+import android.os.Bundle
+import android.support.test.InstrumentationRegistry
 import android.support.test.runner.AndroidJUnitRunner
+import com.linkedin.android.testbutler.TestButler
+
 
 class MockTestRunner : AndroidJUnitRunner() {
+  override fun onStart() {
+    TestButler.setup(InstrumentationRegistry.getTargetContext())
+    super.onStart()
+  }
+
+  override fun finish(resultCode: Int, results: Bundle) {
+    TestButler.teardown(InstrumentationRegistry.getTargetContext())
+    super.finish(resultCode, results)
+  }
+
   @Throws(InstantiationException::class, IllegalAccessException::class, ClassNotFoundException::class)
   override fun newApplication(cl: ClassLoader, className: String, context: Context): Application {
     return super.newApplication(cl, MockApplication::class.java.getName(), context)
