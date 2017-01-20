bad77f9edd27c543766e0c129496afd1a31a436e
Konstantinos Paparas <kelsos86@gmail.com>
2017-01-20 21:21:18 +0200

    Adds option to play track and queue all the list #100

--- app/src/main/kotlin/com/kelsos/mbrc/services/QueueService.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/services/QueueService.kt
@@ -5,5 +5,5 @@ import com.kelsos.mbrc.data.QueueResponse
 import rx.Single
 
 interface QueueService {
-  fun queue(@QueueType type: String, tracks: List<String>): Single<QueueResponse>
+  fun queue(@QueueType type: String, tracks: List<String>, play: String? = null): Single<QueueResponse>
 }
