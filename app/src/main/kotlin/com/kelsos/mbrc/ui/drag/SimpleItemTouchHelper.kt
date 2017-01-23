c694344a8b9be4469ecf440c23ca91d4d6c07f09
Konstantinos Paparas <kelsos86@gmail.com>
2017-01-24 00:03:31 +0200

    Adds a background to the item in now playing when swiping

--- app/src/main/kotlin/com/kelsos/mbrc/ui/drag/SimpleItemTouchHelper.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/ui/drag/SimpleItemTouchHelper.kt
@@ -1,5 +1,6 @@
 package com.kelsos.mbrc.ui.drag
 
+import android.graphics.Canvas
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.helper.ItemTouchHelper
 
@@ -31,5 +32,39 @@ class SimpleItemTouchHelper(private val adapter: ItemTouchHelperAdapter) : ItemT
 
   override fun isItemViewSwipeEnabled(): Boolean {
     return true
+  }
+
+  override fun onChildDrawOver(c: Canvas?, recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder?, dX: Float, dY: Float, actionState: Int, isCurrentlyActive: Boolean) {
+    if (actionState == ItemTouchHelper.ACTION_STATE_SWIPE) {
+      // Fade out the view as it is swiped out of the parent's bounds
+      val alpha = ALPHA_FULL - Math.abs(dX) / viewHolder!!.itemView.width.toFloat()
+      viewHolder.itemView.alpha = alpha
+      viewHolder.itemView.translationX = dX
+    } else {
+      super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
+    }
+  }
+
+  override fun onSelectedChanged(viewHolder: RecyclerView.ViewHolder?, actionState: Int) {
+    if (actionState != ItemTouchHelper.ACTION_STATE_IDLE) {
+      if (viewHolder is TouchHelperViewHolder) {
+        viewHolder.onItemSelected()
+      }
+    }
+
+    super.onSelectedChanged(viewHolder, actionState)
+  }
+
+  override fun clearView(recyclerView: RecyclerView?, viewHolder: RecyclerView.ViewHolder) {
+    super.clearView(recyclerView, viewHolder)
+    viewHolder.itemView.alpha = ALPHA_FULL
+
+    if (viewHolder is TouchHelperViewHolder) {
+      viewHolder.onItemClear()
+    }
+  }
+
+  companion object {
+    const val ALPHA_FULL = 1.0F
   }
 }
