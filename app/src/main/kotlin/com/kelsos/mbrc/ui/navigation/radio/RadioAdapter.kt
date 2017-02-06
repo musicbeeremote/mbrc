1690fb6935c80ddd0d90b71d4ccdd2ac6d79f50e
Konstantinos Paparas <kelsos86@gmail.com>
2017-02-06 21:34:08 +0200

    Adds basic radio functionality

---
+++ app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/radio/RadioAdapter.kt
@@ -0,0 +1,73 @@
+package com.kelsos.mbrc.ui.navigation.radio
+
+
+import android.app.Activity
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+import butterknife.BindView
+import butterknife.ButterKnife
+import com.kelsos.mbrc.R
+import com.kelsos.mbrc.data.RadioStation
+import com.raizlabs.android.dbflow.list.FlowCursorList
+import javax.inject.Inject
+
+class RadioAdapter
+@Inject constructor(context: Activity) : RecyclerView.Adapter<RadioAdapter.ViewHolder>() {
+
+  private val inflater: LayoutInflater = LayoutInflater.from(context)
+  private var data: FlowCursorList<RadioStation>? = null
+  private var radioPressedListener: OnRadioPressedListener? = null
+
+  override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+    val view = inflater.inflate(R.layout.listitem_single, parent, false)
+    val viewHolder = ViewHolder(view)
+
+    viewHolder.itemView.setOnClickListener {
+      val path = data?.getItem(viewHolder.adapterPosition.toLong())?.url
+      path?.let {
+        radioPressedListener?.onRadioPressed(it)
+      }
+
+    }
+    return viewHolder
+  }
+
+  override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+    val playlist = data?.getItem(holder.adapterPosition.toLong())
+    playlist?.let {
+      holder.name.text = playlist.name
+    }
+    holder.context.visibility = View.GONE
+
+  }
+
+  override fun getItemCount(): Int {
+    return data?.count ?: 0
+  }
+
+  fun update(cursor: FlowCursorList<RadioStation>) {
+    this.data = cursor
+    notifyDataSetChanged()
+  }
+
+  fun setOnRadioPressedListener(onRadioPressedListener: OnRadioPressedListener?) {
+    this.radioPressedListener = onRadioPressedListener
+  }
+
+  interface OnRadioPressedListener {
+    fun onRadioPressed(path: String)
+  }
+
+  class ViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    @BindView(R.id.line_one) lateinit var name: TextView
+    @BindView(R.id.ui_item_context_indicator) lateinit var context: LinearLayout
+
+    init {
+      ButterKnife.bind(this, itemView)
+    }
+  }
+}
