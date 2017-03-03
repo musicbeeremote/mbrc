c97c48f833d26e0cc109b4daa99c31f2c1064bc0
Konstantinos Paparas <kelsos86@gmail.com>
2017-03-03 21:56:24 +0200

    Adds client id information on the handshake

--- app/src/main/kotlin/com/kelsos/mbrc/commands/ProtocolRequest.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/commands/ProtocolRequest.kt
@@ -6,13 +6,17 @@ import com.kelsos.mbrc.data.SocketMessage
 import com.kelsos.mbrc.interfaces.ICommand
 import com.kelsos.mbrc.interfaces.IEvent
 import com.kelsos.mbrc.services.SocketService
+import com.kelsos.mbrc.utilities.SettingsManager
 import javax.inject.Inject
 
 class ProtocolRequest
-@Inject constructor(private val socket: SocketService) : ICommand {
+@Inject constructor(
+    private val socket: SocketService,
+    private val settingsManager: SettingsManager
+) : ICommand {
 
   override fun execute(e: IEvent) {
-    val payload = ProtocolPayload()
+    val payload = ProtocolPayload(settingsManager.getClientId())
     payload.noBroadcast = false
     payload.protocolVersion = Protocol.ProtocolVersionNumber
     socket.sendData(SocketMessage.create(Protocol.ProtocolTag, payload))
--- app/src/main/kotlin/com/kelsos/mbrc/data/ProtocolPayload.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/data/ProtocolPayload.kt
@@ -6,8 +6,16 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder
 
 @JsonInclude(JsonInclude.Include.NON_NULL)
 
-@JsonPropertyOrder("no_broadcast", "protocol_version")
-data class ProtocolPayload(@JsonProperty("no_broadcast")
-                           var noBroadcast: Boolean = false,
-                           @JsonProperty("protocol_version")
-                           var protocolVersion: Int = 3)
+@JsonPropertyOrder(
+    "client_id",
+    "no_broadcast",
+    "protocol_version"
+)
+data class ProtocolPayload(
+    @JsonProperty("client_id")
+    var clientId: String,
+    @JsonProperty("no_broadcast")
+    var noBroadcast: Boolean = false,
+    @JsonProperty("protocol_version")
+    var protocolVersion: Int = 3
+)
