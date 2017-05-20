fc456724421111064f205679de2b85259b079e8c
Konstantinos Paparas <kelsos86@gmail.com>
2017-05-20 21:30:40 +0300

    Adds some extensions for the FlowQuery/Cursor List To get the a default count of 0 when the data is null

--- app/src/main/kotlin/com/kelsos/mbrc/adapters/PlaylistAdapter.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/adapters/PlaylistAdapter.kt
@@ -11,19 +11,16 @@ import butterknife.BindView
 import butterknife.ButterKnife
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.data.Playlist
+import com.kelsos.mbrc.extensions.count
 import com.raizlabs.android.dbflow.list.FlowCursorList
 import javax.inject.Inject
 
 class PlaylistAdapter
 @Inject constructor(context: Activity) : RecyclerView.Adapter<PlaylistAdapter.ViewHolder>() {
 
-  private val inflater: LayoutInflater
+  private val inflater: LayoutInflater = LayoutInflater.from(context)
   private var data: FlowCursorList<Playlist>? = null
   private var playlistPressedListener: OnPlaylistPressedListener? = null
-
-  init {
-    inflater = LayoutInflater.from(context)
-  }
 
   override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
     val view = inflater.inflate(R.layout.listitem_single, parent, false)
@@ -48,9 +45,7 @@ class PlaylistAdapter
 
   }
 
-  override fun getItemCount(): Int {
-    return data?.count?.toInt() ?: 0
-  }
+  override fun getItemCount(): Int = data.count()
 
   fun update(cursor: FlowCursorList<Playlist>) {
     this.data = cursor
