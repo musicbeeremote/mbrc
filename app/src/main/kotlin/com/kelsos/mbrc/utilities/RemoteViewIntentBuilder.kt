1371255ff0472a384e25bfd3ded2dc6f5771f525
Konstantinos Paparas <kelsos86@gmail.com>
2017-05-24 21:32:38 +0300

    Some minor refactoring on the broadcast receiver and intent builder

--- app/src/main/kotlin/com/kelsos/mbrc/utilities/RemoteViewIntentBuilder.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/utilities/RemoteViewIntentBuilder.kt
@@ -1,18 +1,22 @@
 package com.kelsos.mbrc.utilities
 
+import android.annotation.SuppressLint
 import android.app.PendingIntent
 import android.app.PendingIntent.FLAG_UPDATE_CURRENT
+import android.app.PendingIntent.getActivity
+import android.app.PendingIntent.getBroadcast
 import android.content.Context
 import android.content.Intent
 import android.support.annotation.IntDef
 import com.kelsos.mbrc.ui.navigation.main.MainActivity
 
 object RemoteViewIntentBuilder {
-  const val REMOTE_PLAY_PRESSED = "com.kelsos.mbrc.notification.play"
-  const val REMOTE_NEXT_PRESSED = "com.kelsos.mbrc.notification.next"
-  const val REMOTE_CLOSE_PRESSED = "com.kelsos.mbrc.notification.close"
-  const val REMOTE_PREVIOUS_PRESSED = "com.kelsos.mbrc.notification.previous"
+  const val PLAY_PRESSED = "com.kelsos.mbrc.notification.play"
+  const val NEXT_PRESSED = "com.kelsos.mbrc.notification.next"
+  const val CLOSE_PRESSED = "com.kelsos.mbrc.notification.close"
+  const val PREVIOUS_PRESSED = "com.kelsos.mbrc.notification.previous"
   const val CANCELLED_NOTIFICATION = "com.kelsos.mbrc.notification.cancel"
+
   const val OPEN = 0
   const val PLAY = 1
   const val NEXT = 2
@@ -20,60 +24,27 @@ object RemoteViewIntentBuilder {
   const val PREVIOUS = 4
   const val CANCEL = 5
 
-  fun getPendingIntent(@ButtonAction id: Int, mContext: Context): PendingIntent {
-    when (id) {
-      OPEN -> {
-        val notificationIntent = Intent(mContext, MainActivity::class.java)
-        return PendingIntent.getActivity(mContext,
-            0,
-            notificationIntent,
-            FLAG_UPDATE_CURRENT)
-      }
-      PLAY -> {
-        val playPressedIntent = Intent(REMOTE_PLAY_PRESSED)
-        return PendingIntent.getBroadcast(mContext,
-            1,
-            playPressedIntent,
-            FLAG_UPDATE_CURRENT)
-      }
-      NEXT -> {
-        val mediaNextButtonIntent = Intent(REMOTE_NEXT_PRESSED)
-        return PendingIntent.getBroadcast(mContext,
-            2,
-            mediaNextButtonIntent,
-            FLAG_UPDATE_CURRENT)
-      }
-      CLOSE -> {
-        val clearNotificationIntent = Intent(REMOTE_CLOSE_PRESSED)
-        return PendingIntent.getBroadcast(mContext,
-            3,
-            clearNotificationIntent,
-            FLAG_UPDATE_CURRENT)
-      }
-      PREVIOUS -> {
-        val mediaPreviousButtonIntent = Intent(REMOTE_PREVIOUS_PRESSED)
-        return PendingIntent.getBroadcast(mContext,
-            4,
-            mediaPreviousButtonIntent,
-            FLAG_UPDATE_CURRENT)
-      }
-      CANCEL -> {
-        val cancelIntent = Intent(CANCELLED_NOTIFICATION)
-        return PendingIntent.getBroadcast(mContext,
-            4,
-            cancelIntent,
-            FLAG_UPDATE_CURRENT)
-      }
+  @SuppressLint("SwitchIntDef")
+  fun getPendingIntent(@ButtonAction id: Int, context: Context): PendingIntent {
+    return when (id) {
+      OPEN -> getActivity(context, OPEN, Intent(context, MainActivity::class.java), FLAG_UPDATE_CURRENT)
+      PLAY -> getBroadcast(context, PLAY, Intent(PLAY_PRESSED), FLAG_UPDATE_CURRENT)
+      NEXT -> getBroadcast(context, NEXT, Intent(NEXT_PRESSED), FLAG_UPDATE_CURRENT)
+      CLOSE -> getBroadcast(context, CLOSE, Intent(CLOSE_PRESSED), FLAG_UPDATE_CURRENT)
+      PREVIOUS -> getBroadcast(context, PREVIOUS, Intent(PREVIOUS_PRESSED), FLAG_UPDATE_CURRENT)
+      CANCEL -> getBroadcast(context, CANCEL, Intent(CANCELLED_NOTIFICATION), FLAG_UPDATE_CURRENT)
       else -> throw IndexOutOfBoundsException()
     }
   }
 
-  @IntDef(OPEN.toLong(),
+  @IntDef(
+      OPEN.toLong(),
       PLAY.toLong(),
       CLOSE.toLong(),
       PREVIOUS.toLong(),
       NEXT.toLong(),
-      CANCEL.toLong())
+      CANCEL.toLong()
+  )
   @kotlin.annotation.Retention(AnnotationRetention.SOURCE)
   annotation class ButtonAction
 }
