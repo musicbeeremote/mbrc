93aa6d1aed98716ecf33b4074e3a2402d450b0bb
Konstantinos Paparas <kelsos86@gmail.com>
2017-09-07 21:51:48 +0300

    WIP: on library filtering functionality

---
+++ app/src/main/kotlin/com/kelsos/mbrc/content/library/albums/Sorting.kt
@@ -0,0 +1,28 @@
+package com.kelsos.mbrc.content.library.albums
+
+import android.support.annotation.IntDef
+
+object Sorting {
+  const val ALBUM = 1L
+  const val ALBUM_ARTIST__ALBUM = 2L
+  const val ALBUM_ARTIST__YEAR__ALBUM = 3L
+  const val ARTIST__ALBUM = 4L
+  const val GENRE__ALBUM_ARTIST__ALBUM = 5L
+  const val YEAR__ALBUM = 6L
+  const val YEAR__ALBUM_ARTIST__ALBUM = 7L
+
+  /**
+   * There order in which the albums appear sorted to the user.
+   */
+  @IntDef(
+      ALBUM,
+      ALBUM_ARTIST__ALBUM,
+      ALBUM_ARTIST__YEAR__ALBUM,
+      ARTIST__ALBUM,
+      GENRE__ALBUM_ARTIST__ALBUM,
+      YEAR__ALBUM,
+      YEAR__ALBUM_ARTIST__ALBUM
+  )
+  @Retention(AnnotationRetention.SOURCE)
+  annotation class Order
+}
