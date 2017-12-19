e961acd7c77bd176979c892f467ba011071eb04c
Konstantinos Paparas <kelsos86@gmail.com>
2017-12-19 21:23:42 +0200

    refactor: updates radio activity layout

--- app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/radio/RadioActivity.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/ui/navigation/radio/RadioActivity.kt
@@ -1,14 +1,13 @@
 package com.kelsos.mbrc.ui.navigation.radio
 
 import android.os.Bundle
-import android.view.View
+import androidx.core.view.isGone
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.swiperefreshlayout.widget.SwipeRefreshLayout
 import com.google.android.material.snackbar.Snackbar
 import com.kelsos.mbrc.R
 import com.kelsos.mbrc.content.radios.RadioStation
 import com.kelsos.mbrc.databinding.ActivityRadioBinding
-import com.kelsos.mbrc.databinding.ListEmptyViewBinding
 import com.kelsos.mbrc.ui.activities.BaseNavigationActivity
 import com.kelsos.mbrc.ui.navigation.radio.RadioAdapter.OnRadioPressedListener
 import toothpick.Scope
@@ -25,13 +24,10 @@ class RadioActivity :
   @Inject lateinit var presenter: RadioPresenter
   @Inject lateinit var adapter: RadioAdapter
 
-  override fun active(): Int {
-    return R.id.nav_radio
-  }
+  override fun active(): Int = R.id.nav_radio
 
   private lateinit var scope: Scope
   private lateinit var binding: ActivityRadioBinding
-  private lateinit var emptyBinding: ListEmptyViewBinding
 
   override fun onCreate(savedInstanceState: Bundle?) {
     Toothpick.openScope(PRESENTER_SCOPE).installModules(RadioModule())
@@ -39,18 +35,13 @@ class RadioActivity :
     scope.installModules(SmoothieActivityModule(this))
     super.onCreate(savedInstanceState)
     binding = ActivityRadioBinding.inflate(layoutInflater)
-    emptyBinding = ListEmptyViewBinding.bind(binding.root)
     setContentView(binding.root)
     Toothpick.inject(this, scope)
 
     super.setup()
-    binding.swipeLayout.setOnRefreshListener(this)
-    binding.swipeLayout.setSwipeableChildren(R.id.radio_list, R.id.empty_view)
-    emptyBinding.listEmptyTitle.setText(R.string.radio__no_radio_stations)
-    emptyBinding.listEmptyIcon.setImageResource(R.drawable.ic_radio_black_80dp)
-    binding.radioList.adapter = adapter
-    binding.radioList.emptyView = emptyBinding.emptyView
-    binding.radioList.layoutManager = LinearLayoutManager(this)
+    binding.radioStationsRefreshLayout.setOnRefreshListener(this)
+    binding.radioStationsStationsList.adapter = adapter
+    binding.radioStationsStationsList.layoutManager = LinearLayoutManager(this)
     presenter.attach(this)
     presenter.load()
     adapter.setOnRadioPressedListener(this)
@@ -68,11 +59,12 @@ class RadioActivity :
   }
 
   override fun update(data: List<RadioStation>) {
+    binding.radioStationsEmptyGroup.isGone = data.isEmpty()
     adapter.update(data)
   }
 
   override fun error(error: Throwable) {
-    Snackbar.make(binding.radioList, R.string.radio__loading_failed, Snackbar.LENGTH_SHORT).show()
+    Snackbar.make(binding.root, R.string.radio__loading_failed, Snackbar.LENGTH_SHORT).show()
   }
 
   override fun onRadioPressed(path: String) {
@@ -84,26 +76,19 @@ class RadioActivity :
   }
 
   override fun radioPlayFailed() {
-    Snackbar.make(binding.radioList, R.string.radio__play_failed, Snackbar.LENGTH_SHORT).show()
+    Snackbar.make(binding.root, R.string.radio__play_failed, Snackbar.LENGTH_SHORT).show()
   }
 
   override fun radioPlaySuccessful() {
-    Snackbar.make(binding.radioList, R.string.radio__play_successful, Snackbar.LENGTH_SHORT).show()
+    Snackbar.make(binding.root, R.string.radio__play_successful, Snackbar.LENGTH_SHORT).show()
   }
 
   override fun showLoading() {
-    emptyBinding.emptyViewProgressBar.visibility = View.VISIBLE
-    emptyBinding.listEmptyIcon.visibility = View.GONE
-    emptyBinding.listEmptyTitle.visibility = View.GONE
-    emptyBinding.listEmptySubtitle.visibility = View.GONE
   }
 
   override fun hideLoading() {
-    emptyBinding.emptyViewProgressBar.visibility = View.GONE
-    emptyBinding.listEmptyIcon.visibility = View.VISIBLE
-    emptyBinding.listEmptyTitle.visibility = View.VISIBLE
-    emptyBinding.listEmptySubtitle.visibility = View.VISIBLE
-    binding.swipeLayout.isRefreshing = false
+    binding.radioStationsLoadingBar.isGone = true
+    binding.radioStationsRefreshLayout.isRefreshing = false
   }
 
   @javax.inject.Scope
