2026c85a53b00404ca80f576f51ee195bf1ca07c
Konstantinos Paparas <kelsos86@gmail.com>
2019-04-26 23:48:45 +0200

    Merging interface and implementation to single file

--- app/src/main/java/com/kelsos/mbrc/features/widgets/WidgetUpdater.kt
+++ app/src/main/java/com/kelsos/mbrc/features/widgets/WidgetUpdater.kt
@@ -1,5 +1,9 @@
 package com.kelsos.mbrc.features.widgets
 
+import android.appwidget.AppWidgetManager
+import android.content.Context
+import android.content.Intent
+import com.kelsos.mbrc.content.activestatus.PlayerState
 import com.kelsos.mbrc.content.library.tracks.PlayingTrack
 
 interface WidgetUpdater {
@@ -14,5 +18,51 @@ interface WidgetUpdater {
     const val INFO = "com.kelsos.mbrc.features.widgets.INFO"
     const val TRACK_INFO = "com.kelsos.mbrc.features.widgets.TRACKINFO"
     const val PLAYER_STATE = "com.kelsos.mbrc.features.widgets.PLAYER_STATE"
+  }
+}
+
+class WidgetUpdaterImpl(
+  private val context: Context
+) : WidgetUpdater {
+
+  private fun createIntent(clazz: Class<*>): Intent {
+    val widgetUpdateIntent = Intent(context, clazz)
+    widgetUpdateIntent.action = AppWidgetManager.ACTION_APPWIDGET_UPDATE
+    return widgetUpdateIntent
+  }
+
+  private fun Intent.payload(track: PlayingTrack): Intent {
+    return putExtra(WidgetUpdater.INFO, true).putExtra(WidgetUpdater.TRACK_INFO, track)
+  }
+
+  private fun Intent.payload(path: String): Intent {
+    return putExtra(WidgetUpdater.COVER, true).putExtra(WidgetUpdater.COVER_PATH, path)
+  }
+
+  private fun Intent.statePayload(@PlayerState.State state: String): Intent {
+    return putExtra(WidgetUpdater.STATE, true).putExtra(WidgetUpdater.PLAYER_STATE, state)
+  }
+
+  override fun updatePlayingTrack(track: PlayingTrack) {
+    val normalIntent = createIntent(WidgetNormal::class.java).payload(track)
+    val smallIntent = createIntent(WidgetSmall::class.java).payload(track)
+    broadcast(smallIntent, normalIntent)
+  }
+
+  override fun updatePlayState(state: String) {
+    val normalIntent = createIntent(WidgetNormal::class.java).statePayload(state)
+    val smallIntent = createIntent(WidgetSmall::class.java).statePayload(state)
+    broadcast(smallIntent, normalIntent)
+  }
+
+  override fun updateCover(path: String) {
+    val normalIntent = createIntent(WidgetNormal::class.java).payload(path)
+    val smallIntent = createIntent(WidgetSmall::class.java).payload(path)
+    broadcast(smallIntent, normalIntent)
+  }
+
+  private fun broadcast(smallIntent: Intent, normalIntent: Intent) = with(context) {
+    sendBroadcast(smallIntent)
+    sendBroadcast(normalIntent)
   }
 }
