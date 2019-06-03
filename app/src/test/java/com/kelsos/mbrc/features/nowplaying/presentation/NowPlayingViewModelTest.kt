364ab3ac7ab7d31032e84a98924f28a69a1e01c2
Konstantinos Paparas <kelsos86@gmail.com>
2019-06-04 00:17:18 +0200

    Adds NowPlayingViewModelTests

--- app/src/test/java/com/kelsos/mbrc/features/nowplaying/presentation/NowPlayingViewModelTest.kt
+++ app/src/test/java/com/kelsos/mbrc/features/nowplaying/presentation/NowPlayingViewModelTest.kt
@@ -6,9 +6,12 @@ import com.google.common.truth.Truth.assertThat
 import com.kelsos.mbrc.TestApplication
 import com.kelsos.mbrc.content.activestatus.livedata.PlayingTrackState
 import com.kelsos.mbrc.events.Event
+import com.kelsos.mbrc.events.UserAction
 import com.kelsos.mbrc.features.nowplaying.domain.MoveManagerImpl
 import com.kelsos.mbrc.features.nowplaying.repository.NowPlayingRepository
 import com.kelsos.mbrc.networking.client.UserActionUseCase
+import com.kelsos.mbrc.networking.protocol.NowPlayingMoveRequest
+import com.kelsos.mbrc.networking.protocol.Protocol
 import com.kelsos.mbrc.utils.MockFactory
 import com.kelsos.mbrc.utils.TestDispatchers
 import com.kelsos.mbrc.utils.observeOnce
@@ -20,13 +23,15 @@ import io.mockk.just
 import io.mockk.mockk
 import io.mockk.slot
 import io.mockk.verify
-import org.junit.After
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 import org.robolectric.annotation.Config
 import java.net.SocketTimeoutException
 
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 @Config(application = TestApplication::class)
 class NowPlayingViewModelTest {
@@ -56,10 +61,6 @@ class NowPlayingViewModelTest {
     )
   }
 
-  @After
-  fun tearDown() {
-  }
-
   @Test
   fun `should notify the observer that refresh failed`() {
     coEvery { repository.getRemote() } coAnswers { Try.raiseError(SocketTimeoutException()) }
@@ -76,5 +77,48 @@ class NowPlayingViewModelTest {
     viewModel.reload()
     verify(exactly = 1) { observer(any()) }
     assertThat(slot.captured.peekContent()).isEqualTo(NowPlayingUiMessages.RefreshSuccess)
+  }
+
+  @Test
+  fun `move should commit the cached move instructions`() {
+    val actionSlot = slot<UserAction>()
+    every { userActionUseCase.perform(capture(actionSlot)) } just Runs
+    viewModel.moveTrack(0, 1)
+    viewModel.moveTrack(1, 2)
+    viewModel.moveTrack(2, 3)
+    viewModel.move()
+
+    assertThat(actionSlot.captured.context).isEqualTo(Protocol.NowPlayingListMove)
+    assertThat(actionSlot.captured.data).isEqualTo(NowPlayingMoveRequest(0, 3))
+  }
+
+  @Test
+  fun `search should play perform user action`() {
+    val actionSlot = slot<UserAction>()
+    every { userActionUseCase.perform(capture(actionSlot)) } just Runs
+    coEvery { repository.findPosition(any()) } answers { 5 }
+    viewModel.search("search")
+
+    assertThat(actionSlot.captured.context).isEqualTo(Protocol.NowPlayingListPlay)
+    assertThat(actionSlot.captured.data).isEqualTo(6)
+  }
+
+  @Test
+  fun `remove should perform user action`() = runBlockingTest {
+    val actionSlot = slot<UserAction>()
+    every { userActionUseCase.perform(capture(actionSlot)) } just Runs
+    viewModel.removeTrack(1)
+    assertThat(actionSlot.captured.context).isEqualTo(Protocol.NowPlayingListRemove)
+    assertThat(actionSlot.captured.data).isEqualTo(1)
+  }
+
+  @Test
+  fun `play should perform user action`() {
+    val actionSlot = slot<UserAction>()
+    every { userActionUseCase.perform(capture(actionSlot)) } just Runs
+    viewModel.play(2)
+
+    assertThat(actionSlot.captured.context).isEqualTo(Protocol.NowPlayingListPlay)
+    assertThat(actionSlot.captured.data).isEqualTo(3)
   }
 }
