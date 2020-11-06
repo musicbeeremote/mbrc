0f9c905193e0949e6488dca8806e9e580619248d
Konstantinos Paparas <kelsos86@gmail.com>
2020-11-06 21:15:45 +0100

    Updates model cache to use datastore

--- app/src/main/kotlin/com/kelsos/mbrc/repository/ModelCache.kt
+++ app/src/main/kotlin/com/kelsos/mbrc/repository/ModelCache.kt
@@ -1,91 +1,82 @@
 package com.kelsos.mbrc.repository
 
 import android.app.Application
-import com.fasterxml.jackson.databind.ObjectMapper
+import androidx.datastore.DataStore
+import androidx.datastore.createDataStore
 import com.kelsos.mbrc.di.modules.AppDispatchers
 import com.kelsos.mbrc.domain.TrackInfo
+import com.kelsos.mbrc.store.Store
+import com.kelsos.mbrc.store.StoreSerializer
+import com.kelsos.mbrc.store.Track
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.catch
+import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.withContext
-import java.io.BufferedReader
-import java.io.BufferedWriter
-import java.io.File
-import java.io.FileNotFoundException
-import java.io.FileReader
-import java.io.FileWriter
+import timber.log.Timber
+import java.io.IOException
 import javax.inject.Inject
 
 class ModelCacheImpl
 @Inject
 constructor(
-  private val mapper: ObjectMapper,
-  private val context: Application,
+  context: Application,
   private val dispatchers: AppDispatchers
 ) : ModelCache {
 
-  private fun writeTrackInfo(infoFile: File, trackInfo: TrackInfo) {
-    mapper.writeValue(infoFile, trackInfo)
-  }
+  private val dataStore: DataStore<Store> =
+    context.createDataStore("cache_store.db", StoreSerializer)
+  private val storeFlow: Flow<Store> = dataStore.data
+    .catch { exception ->
+      // dataStore.data throws an IOException when an error is encountered when reading data
+      if (exception is IOException) {
+        Timber.e(exception, "Error reading sort order preferences.")
+        emit(Store.getDefaultInstance())
+      } else {
+        throw exception
+      }
+    }
 
-  private fun readTrackInfo(infoFile: File) =
-    mapper.readValue(infoFile, TrackInfo::class.java)
 
   override suspend fun persistInfo(trackInfo: TrackInfo) = withContext(dispatchers.io) {
-    val infoFile = File(context.filesDir, TRACK_INFO)
-    if (infoFile.exists()) {
-      infoFile.delete()
+    dataStore.updateData { store ->
+      val track = Track.newBuilder()
+        .setAlbum(trackInfo.album)
+        .setArtist(trackInfo.artist)
+        .setPath(trackInfo.path)
+        .setTitle(trackInfo.title)
+        .setYear(trackInfo.year)
+        .build()
+
+      store.toBuilder()
+        .setTrack(track)
+        .build()
     }
-    writeTrackInfo(infoFile, trackInfo)
+    return@withContext
   }
 
   override suspend fun restoreInfo(): TrackInfo = withContext(dispatchers.io) {
-    val infoFile = File(context.filesDir, TRACK_INFO)
-    if (infoFile.exists()) {
-      return@withContext readTrackInfo(infoFile)
-    }
-    throw FileNotFoundException()
+    val track = storeFlow.first().track
+
+    return@withContext TrackInfo(
+      track.artist,
+      track.title,
+      track.album,
+      track.year,
+      track.path
+    )
   }
 
   override suspend fun persistCover(cover: String) {
-    withContext(dispatchers.io) {
-      val coverFile = File(context.filesDir, COVER_INFO)
-      if (coverFile.exists()) {
-        coverFile.delete()
-      }
-
-      writeCover(coverFile, cover)
+    dataStore.updateData { store ->
+      store.toBuilder().setCover(cover).build()
     }
   }
 
-  private fun writeCover(coverFile: File, cover: String) {
-    FileWriter(coverFile).use { fileWriter: FileWriter ->
-      BufferedWriter(fileWriter).use { bufferedWriter ->
-        bufferedWriter.write(cover)
-        bufferedWriter.flush()
-      }
-    }
-  }
 
   override suspend fun restoreCover(): String {
-    return withContext(dispatchers.io) {
-      val coverFile = File(context.filesDir, COVER_INFO)
-      if (coverFile.exists()) {
-        return@withContext readCover(coverFile)
-      }
-      return@withContext ""
-    }
+    return storeFlow.first().cover
   }
 
-  private fun readCover(coverFile: File): String {
-    FileReader(coverFile).use { fileReader ->
-      BufferedReader(fileReader).use { bufferedReader ->
-        return bufferedReader.readLine()
-      }
-    }
-  }
-
-  companion object {
-    const val TRACK_INFO = "track.json"
-    const val COVER_INFO = "cover.txt"
-  }
 }
 
 interface ModelCache {
---
+++ app/src/main/kotlin/com/kelsos/mbrc/store/StoreSerializer.kt
@@ -0,0 +1,20 @@
+package com.kelsos.mbrc.store
+
+import androidx.datastore.CorruptionException
+import androidx.datastore.Serializer
+import com.google.protobuf.InvalidProtocolBufferException
+import java.io.InputStream
+import java.io.OutputStream
+
+object StoreSerializer : Serializer<Store> {
+  override fun readFrom(input: InputStream): Store {
+    try {
+      return Store.parseFrom(input)
+    } catch (exception: InvalidProtocolBufferException) {
+      throw CorruptionException("Cannot read proto.", exception)
+    }
+  }
+
+  override fun writeTo(t: Store, output: OutputStream) = t.writeTo(output)
+
+}
