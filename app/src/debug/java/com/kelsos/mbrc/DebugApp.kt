75048b9577f094ba700e6209d62c27416a81b2a7
Konstantinos Paparas <kelsos86@gmail.com>
2020-11-11 01:08:55 +0100

    Updates or removes any dependencies that needed jetifier

--- app/src/debug/java/com/kelsos/mbrc/DebugApp.kt
+++ app/src/debug/java/com/kelsos/mbrc/DebugApp.kt
@@ -1,10 +1,8 @@
 package com.kelsos.mbrc
 
-import com.facebook.stetho.Stetho
 
 class DebugApp : FlavorApp() {
   override fun onCreate() {
     super.onCreate()
-    Stetho.initializeWithDefaults(this)
   }
 }
