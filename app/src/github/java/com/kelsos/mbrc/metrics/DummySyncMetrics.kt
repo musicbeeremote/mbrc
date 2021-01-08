e18935b6c94c42cfc2af7cc2cf1aa596a39747d0
Konstantinos Paparas <kelsos86@gmail.com>
2021-01-08 01:22:57 +0100

    chore: adds dummy sync metrics implementation

--- app/src/github/java/com/kelsos/mbrc/metrics/DummySyncMetrics.kt
+++ app/src/github/java/com/kelsos/mbrc/metrics/DummySyncMetrics.kt
@@ -1,13 +1,22 @@
 package com.kelsos.mbrc.metrics
 
+import timber.log.Timber
+
 class DummySyncMetrics : SyncMetrics {
 
+  var start = 0L
+
   override fun librarySyncComplete(metrics: SyncedData) {
+    val total = System.currentTimeMillis() - start
+    Timber.v("Total sync time $total ms for the following $metrics")
   }
 
   override fun librarySyncStarted() {
+    Timber.v("Sync metrics timer started")
+    start = System.currentTimeMillis()
   }
 
   override fun librarySyncFailed() {
+    Timber.v("Ignoring metrics due to failure")
   }
 }
