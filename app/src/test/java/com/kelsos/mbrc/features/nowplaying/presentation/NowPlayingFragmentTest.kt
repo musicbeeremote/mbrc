androiddevnotesforks/android-app-1

test: adds tests for the now playing feature

app/src/test/java/com/kelsos/mbrc/features/nowplaying/presentation/NowPlayingFragmentTest.kt
@@ -4,7 +4,10 @@ import androidx.annotation.StringRes
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.action.ViewActions.swipeRight
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -13,13 +16,17 @@ import com.kelsos.mbrc.content.activestatus.livedata.PlayingTrackState
 import com.kelsos.mbrc.features.nowplaying.domain.NowPlaying
 import com.kelsos.mbrc.ui.minicontrol.MiniControlViewModel
 import com.kelsos.mbrc.utils.MockFactory
+import com.kelsos.mbrc.utils.TestDataFactories
 import com.kelsos.mbrc.utils.isGone
 import com.kelsos.mbrc.utils.isVisible
+import com.kelsos.mbrc.utils.testDispatcher
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flow
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -67,8 +74,8 @@ class NowPlayingFragmentTest {
 
   @Test
   fun `when no data shows empty view with message`() {
-    val liveData = MockFactory<NowPlaying>().flow()
-    every { viewModel.list } answers { liveData }
+    val data = MockFactory<NowPlaying>().flow()
+    every { viewModel.list } answers { data }
     every { viewModel.emitter } answers { emptyFlow() }
     launchFragmentInContainer<NowPlayingFragment>()
 
@@ -90,11 +97,107 @@ class NowPlayingFragmentTest {
       .emptyGroupGone()
       .loadingVisible()
   }
+
+  @Test
+  fun `after loading displays now playing`() {
+    val data = MockFactory(
+      TestDataFactories.nowPlayingListEntities(10)
+    ).flow()
+    every { viewModel.list } answers { data }
+    every { viewModel.emitter } answers { emptyFlow() }
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .done()
+      .listVisible()
+      .textVisible("Test title 6")
+  }
+
+  @Test
+  fun `clicking on a track should play it`() {
+    val data = MockFactory(
+      TestDataFactories.nowPlayingListEntities(10)
+    ).flow()
+    every { viewModel.list } answers { data }
+    every { viewModel.emitter } answers { emptyFlow() }
+    every { viewModel.play(any()) } just Runs
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .press(5)
+      .done()
+      .listVisible()
+
+    verify(exactly = 1) { viewModel.play(5) }
+  }
+
+  @Test
+  fun `swiping right on a track should remove it`() {
+    val data = MockFactory(
+      TestDataFactories.nowPlayingListEntities(10)
+    ).flow()
+    every { viewModel.list } answers { data }
+    every { viewModel.emitter } answers { emptyFlow() }
+    every { viewModel.removeTrack(any()) } just Runs
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .swipe(5)
+      .done()
+      .listVisible()
+
+    testDispatcher.advanceUntilIdle()
+
+    verify(exactly = 1) { viewModel.removeTrack(5) }
+  }
+
+  @Test
+  fun `show a queue success message when queue succeeds`() {
+    val events = flow { emit(NowPlayingUiMessages.RefreshSuccess) }
+
+    every { viewModel.list } answers { emptyFlow() }
+    every { viewModel.emitter } answers { events }
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .done()
+      .messageDisplayed(R.string.now_playing__refresh_success)
+  }
+
+  @Test
+  fun `show a fail message when refresh fails`() {
+    val events = flow { emit(NowPlayingUiMessages.RefreshFailed) }
+
+    every { viewModel.list } answers { emptyFlow() }
+    every { viewModel.emitter } answers { events }
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .done()
+      .messageDisplayed(R.string.now_playing__refresh_failed)
+  }
 }
 
 class NowPlayingRobot {
   fun done(): NowPlayingRobotResult {
     return NowPlayingRobotResult()
+  }
+
+  fun press(position: Int): NowPlayingRobot {
+    onView(withId(R.id.now_playing__track_list))
+      .perform(actionOnItemAtPosition<NowPlayingTrackViewHolder>(position, click()))
+    return this
+  }
+
+  fun swipe(position: Int): NowPlayingRobot {
+    onView(withId(R.id.now_playing__track_list))
+      .perform(actionOnItemAtPosition<NowPlayingTrackViewHolder>(position, swipeRight()))
+    return this
   }
 }
 
@@ -125,5 +228,20 @@ class NowPlayingRobotResult {
   fun loadingVisible(): NowPlayingRobotResult {
     loading.isVisible()
     return this
+  }
+
+  fun listVisible(): NowPlayingRobotResult {
+    list.isVisible()
+    return this
+  }
+
+  fun textVisible(text: String): NowPlayingRobotResult {
+    onView(withText(text)).isVisible()
+    return this
+  }
+
+  fun messageDisplayed(@StringRes resId: Int) {
+    onView(withId(com.google.android.material.R.id.snackbar_text))
+      .check(matches(withText(resId)))
   }
 }

musicbeeremote/mbrc

app/src/test/java/com/kelsos/mbrc/features/nowplaying/presentation/NowPlayingFragmentTest.kt
@@ -4,7 +4,9 @@ import androidx.annotation.StringRes
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.fragment.app.testing.launchFragmentInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.contrib.RecyclerViewActions.actionOnItemAtPosition
 import androidx.test.espresso.matcher.ViewMatchers.withId
 import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.ext.junit.runners.AndroidJUnit4
@@ -13,13 +15,18 @@ import com.kelsos.mbrc.content.activestatus.livedata.PlayingTrackState
 import com.kelsos.mbrc.features.nowplaying.domain.NowPlaying
 import com.kelsos.mbrc.ui.minicontrol.MiniControlViewModel
 import com.kelsos.mbrc.utils.MockFactory
+import com.kelsos.mbrc.utils.TestDataFactories
 import com.kelsos.mbrc.utils.isGone
 import com.kelsos.mbrc.utils.isVisible
+import com.kelsos.mbrc.utils.swipeToRemove
+import com.kelsos.mbrc.utils.testDispatcher
 import io.mockk.Runs
 import io.mockk.every
 import io.mockk.just
 import io.mockk.mockk
+import io.mockk.verify
 import kotlinx.coroutines.flow.emptyFlow
+import kotlinx.coroutines.flow.flow
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -67,8 +74,8 @@ class NowPlayingFragmentTest {
 
   @Test
   fun `when no data shows empty view with message`() {
-    val liveData = MockFactory<NowPlaying>().flow()
-    every { viewModel.list } answers { liveData }
+    val data = MockFactory<NowPlaying>().flow()
+    every { viewModel.list } answers { data }
     every { viewModel.emitter } answers { emptyFlow() }
     launchFragmentInContainer<NowPlayingFragment>()
 
@@ -90,11 +97,107 @@ class NowPlayingFragmentTest {
       .emptyGroupGone()
       .loadingVisible()
   }
+
+  @Test
+  fun `after loading displays now playing`() {
+    val data = MockFactory(
+      TestDataFactories.nowPlayingListEntities(10)
+    ).flow()
+    every { viewModel.list } answers { data }
+    every { viewModel.emitter } answers { emptyFlow() }
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .done()
+      .listVisible()
+      .textVisible("Test title 6")
+  }
+
+  @Test
+  fun `clicking on a track should play it`() {
+    val data = MockFactory(
+      TestDataFactories.nowPlayingListEntities(10)
+    ).flow()
+    every { viewModel.list } answers { data }
+    every { viewModel.emitter } answers { emptyFlow() }
+    every { viewModel.play(any()) } just Runs
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .press(5)
+      .done()
+      .listVisible()
+
+    verify(exactly = 1) { viewModel.play(5) }
+  }
+
+  @Test
+  fun `swiping right on a track should remove it`() {
+    val data = MockFactory(
+      TestDataFactories.nowPlayingListEntities(10)
+    ).flow()
+    every { viewModel.list } answers { data }
+    every { viewModel.emitter } answers { emptyFlow() }
+    every { viewModel.removeTrack(any()) } just Runs
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .swipe(5)
+      .done()
+      .listVisible()
+
+    testDispatcher.advanceUntilIdle()
+
+    verify(exactly = 1) { viewModel.removeTrack(5) }
+  }
+
+  @Test
+  fun `show a queue success message when queue succeeds`() {
+    val events = flow { emit(NowPlayingUiMessages.RefreshSuccess) }
+
+    every { viewModel.list } answers { emptyFlow() }
+    every { viewModel.emitter } answers { events }
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .done()
+      .messageDisplayed(R.string.now_playing__refresh_success)
+  }
+
+  @Test
+  fun `show a fail message when refresh fails`() {
+    val events = flow { emit(NowPlayingUiMessages.RefreshFailed) }
+
+    every { viewModel.list } answers { emptyFlow() }
+    every { viewModel.emitter } answers { events }
+
+    launchFragmentInContainer<NowPlayingFragment>()
+
+    NowPlayingRobot()
+      .done()
+      .messageDisplayed(R.string.now_playing__refresh_failed)
+  }
 }
 
 class NowPlayingRobot {
   fun done(): NowPlayingRobotResult {
     return NowPlayingRobotResult()
+  }
+
+  fun press(position: Int): NowPlayingRobot {
+    onView(withId(R.id.now_playing__track_list))
+      .perform(actionOnItemAtPosition<NowPlayingTrackViewHolder>(position, click()))
+    return this
+  }
+
+  fun swipe(position: Int): NowPlayingRobot {
+    onView(withId(R.id.now_playing__track_list))
+      .perform(actionOnItemAtPosition<NowPlayingTrackViewHolder>(position, swipeToRemove()))
+    return this
   }
 }
 
@@ -125,5 +228,20 @@ class NowPlayingRobotResult {
   fun loadingVisible(): NowPlayingRobotResult {
     loading.isVisible()
     return this
+  }
+
+  fun listVisible(): NowPlayingRobotResult {
+    list.isVisible()
+    return this
+  }
+
+  fun textVisible(text: String): NowPlayingRobotResult {
+    onView(withText(text)).isVisible()
+    return this
+  }
+
+  fun messageDisplayed(@StringRes resId: Int) {
+    onView(withId(com.google.android.material.R.id.snackbar_text))
+      .check(matches(withText(resId)))
   }
 }